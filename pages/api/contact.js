2db295ea4ebf234ccc40988c20beb7d043a74872
Will Kerry <mail@example.com>
2021-08-01 16:45:24 +0200

    Change SMTP port

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,7 +4,6 @@ import { getOfficerByHash } from "../../lib/officer-contacts";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
