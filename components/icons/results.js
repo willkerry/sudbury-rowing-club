df2446c2ef17ad787f7253cb9cde566d8fb7e88b
Will Kerry <mail@example.com>
2021-08-02 22:13:52 +0200

    Results page

---
+++ components/icons/results.js
@@ -0,0 +1,25 @@
+const React = require("react");
+
+function Results(props) {
+  return /*#__PURE__*/ React.createElement(
+    "svg",
+    Object.assign(
+      {
+        xmlns: "http://www.w3.org/2000/svg",
+        fill: "none",
+        viewBox: "0 0 24 24",
+        stroke: "currentColor",
+      },
+      props
+    ),
+    /*#__PURE__*/ React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      strokeWidth: 2,
+      d:
+        "M6 3l-.5 18m13 0L18 3M6 7h12M6 11h12m0-6h3a2 2 0 012 2v6a2 2 0 01-2 2h-2.5M5 3h14M4.5 21h15",
+    })
+  );
+}
+
+module.exports = Results;
---
+++ pages/regatta/results.js
@@ -0,0 +1,89 @@
+import Head from "next/head";
+import Container from "@/components/container";
+import HeroTitle from "@/components/hero-title";
+import Layout from "@/components/layout";
+import data from "@/data/regatta";
+import ordinal from "ordinal";
+import Link from "next/link";
+import { ExternalLink } from "react-feather";
+import DateFormatter from "@/components/date-formatter";
+
+export default function Photography({ preview }) {
+  const resultsData = data.results.results;
+  const courseRecords = data.results.courseRecords;
+  return (
+    <Layout preview={preview}>
+      <Head>
+        <title>Sudbury Rowing Club Regatta Results</title>
+      </Head>
+      <HeroTitle title="Regatta results" breadcrumbs />
+      <Container>
+        <div className="py-16 prose prose-lg max-w-none">
+          <table>
+            <thead>
+              <th></th>
+              <th>Date</th>
+              <th>Fastest 350m</th>
+              <th>Fastest 650m</th>
+              <th>Results</th>
+            </thead>
+            <tbody>
+              {resultsData.map(
+                ({
+                  link,
+                  year,
+                  date,
+                  fastest,
+                  eightsFastest,
+                  number,
+                  index,
+                }) => (
+                  <Link key={index} href={link}>
+                    <tr className="cursor-pointer hover:bg-gray-50 group">
+                      <td>
+                        <span className="font-semibold">
+                          {ordinal(number)} Sudbury Regatta
+                        </span>
+                      </td>
+                      <td>
+                        <span className="tabular-nums">
+                          <DateFormatter dateString={date} />
+                        </span>
+                      </td>
+                      <td>
+                        <span className="font-semibold text-pink-500 tabular-nums ">
+                          {eightsFastest}
+                        </span>
+                      </td>
+                      <td>
+                        <span className="font-semibold text-pink-500 tabular-nums ">
+                          {fastest}
+                        </span>
+                      </td>
+                      <td>
+                        {link ? (
+                          <Link href={link}>
+                            <a
+                              className="inline-block pr-4 last-of-type:pr-0 group-hover:text-blue-300"
+                              title={"View the " + year + " regatta results."}
+                            >
+                              View results
+                              <ExternalLink
+                                size={12}
+                                className="inline mb-1 ml-1"
+                              />
+                            </a>
+                          </Link>
+                        ) : null}
+                      </td>
+                    </tr>
+                  </Link>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
