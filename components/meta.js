f9626c827e99f8f67aa5930ee7172111ad0b3087
Will Kerry <mail@example.com>
2021-09-02 13:49:05 +0100

    SEO

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 
 export default function Meta() {
   return (
@@ -31,12 +30,6 @@ export default function Meta() {
       <meta name="msapplication-TileColor" content="#003B80" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#003B80" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   );
 }
