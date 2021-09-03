42d2671e59b67708abc07c5f2fc0802679fd7354
Will Kerry <mail@example.com>
2021-09-03 10:32:20 +0100

    Smaller post title

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="mb-12 text-3xl font-bold leading-tight tracking-tighter text-gray-900 md:text-5xl lg:text-7xl md:leading-none">
+    <h1 className="mb-12 text-3xl font-bold leading-tight tracking-tighter text-gray-900 md:text-5xl lg:text-6xl md:leading-none">
       {children}
     </h1>
   );
