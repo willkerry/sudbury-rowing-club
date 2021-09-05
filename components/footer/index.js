116c2dd662e7425ac50cf1dfc484e15fbe913449
Will Kerry <mail@example.com>
2021-09-05 18:15:29 +0100

    Small reworking of footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -16,8 +16,16 @@ const footerRegatta = [...regatta, ...regattaCTAs];
 const communityFooter = [...memberCTAs, ...socials];
 
 export default function Footer() {
+  function FooterColumn({ heading, data }) {
+    return (
+      <div className="w-1/2 sm:w-4/12 md:w-3/12">
+        <FooterHeading>{heading}</FooterHeading>
+        <FooterItemList data={data} />
+      </div>
+    );
+  }
   const FooterHeading = ({ children }) => (
-    <div className="mb-4 text-xs font-bold tracking-widest uppercase opacity-80 text-blue-50">
+    <div className="mb-4 text-xs font-semibold tracking-widest text-blue-100 uppercase select-none">
       {children}
     </div>
   );
@@ -27,7 +35,7 @@ export default function Footer() {
         <Link key={index} href={item.href} passHref>
           <a
             href="#"
-            className="block my-3 text-sm font-medium duration-100 text-blue-50 opacity-70 hover:opacity-100"
+            className="block my-3 text-sm font-medium text-white duration-100 hover:text-blue-100"
           >
             {item.shortName ? item.shortName : item.name}
           </a>
@@ -47,13 +55,13 @@ export default function Footer() {
     });
   };
   return (
-    <footer className="text-white bg-blue-900 border-t">
+    <footer className="text-white bg-blue-900">
       <Container>
         <div className="flex flex-wrap py-24 justify-left">
-          <div className="w-1/2 sm:w-4/12 md:w-3/12 text-blue-50 opacity-60">
-            <Crest className="h-12 mb-6 md:h-24" />
-            <div className="space-y-1 text-sm">
-              <p className="font-semibold">Sudbury Rowing Club</p>
+          <div className="w-1/2 text-blue-100 sm:w-4/12 md:w-3/12">
+            <Crest className="h-12 mb-6 opacity-80 md:h-24" />
+            <div className="space-y-1 text-sm font-medium">
+              <p className="font-semibold text-blue-50">Sudbury Rowing Club</p>
               <p>Quay Lane</p>
               <p>Sudbury</p>
               <p>CO10 2AN</p>
@@ -61,37 +69,30 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex flex-wrap pb-24 justify-left">
-          <div className="w-1/2 sm:w-4/12 md:w-3/12">
-            <FooterHeading>About</FooterHeading>
-            <FooterItemList data={footerAbout} />
-          </div>
-
-          <div className="w-1/2 sm:w-4/12 md:w-3/12">
-            <FooterHeading>Regatta</FooterHeading>
-            <FooterItemList data={footerRegatta} />
-          </div>
-
-          <div className="w-1/2 sm:w-4/12 md:w-3/12">
-            <FooterHeading>Community</FooterHeading>
-            <FooterItemList data={communityFooter} />
-          </div>
-          <div className="w-1/2 sm:w-4/12 md:w-3/12">
-            <FooterHeading>Miscellany</FooterHeading>
-            <FooterItemList data={misc} />
-          </div>
+          <FooterColumn heading="About" data={footerAbout} />
+          <FooterColumn heading="Regatta" data={footerRegatta} />
+          <FooterColumn heading="Community" data={communityFooter} />
+          <FooterColumn heading="Miscellany" data={misc} />
         </div>
+
         <div className="pt-2">
-          <div className="flex flex-col py-5 m-auto text-sm md:flex-row">
+          <div className="flex flex-col justify-between py-5 m-auto text-sm md:flex-row">
+            <div className="mt-2">© Sudbury Rowing Club 2021.</div>
             <div className="mt-2">
-              © Sudbury Rowing Club 2021. This site is open-source and was{" "}
               <Link href="https://willkerry.com/">
-                <a className="transition opacity-60 hover:opacity-100">
-                  provided for free
+                <a className="mx-1.5 text-blue-100 transition  hover:text-white">
+                  <svg
+                    viewBox="0 0 24 24"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="inline-flex mx-px mb-0.5"
+                  >
+                    <path d="M0 5h3.5l4 9.5L6 18H5zM7 5h3.5l4 9.5L13 18h-1zM16 13.5l4 4.5h4l-6.5-8zM18 8.5L19.5 5H24l-5 3.5z"></path>
+                  </svg>
                 </a>
               </Link>
-              .
             </div>
-
             <div className="flex flex-row mt-2 md:flex-auto md:flex-row-reverse">
               <SocialIcons data={socials} />
             </div>
