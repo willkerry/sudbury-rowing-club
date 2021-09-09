7e64cd3f321381e2e77766ffc5bbaa7fe8992998
Will Kerry <mail@example.com>
2021-09-09 06:56:03 +0100

    Better? user flyoiut menu icon

---
+++ components/icons/user-dropdown.js
@@ -0,0 +1,23 @@
+function UserDropdown(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2M9 11a4 4 0 100-8 4 4 0 000 8zM23 21v-2a4 4 0 00-3-3.9M16 3.1a4 4 0 010 7.8" />
+      <circle cx="5" cy="19" r=".4" />
+      <circle cx="9" cy="19" r=".4" />
+      <circle cx="13" cy="19" r=".4" />
+    </svg>
+  );
+}
+
+export default UserDropdown;
