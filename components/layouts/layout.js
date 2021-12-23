0aec9af04e105ccd339aed25472070b364f57884
Will Kerry <mail@example.com>
2021-12-23 08:35:14 +0000

    Refactor for tidier directory structure

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -2,13 +2,11 @@ import PropTypes from "prop-types";
 import Footer from "@/components/footer";
 import Meta from "@/components/meta";
 import Navbar from "@/components/nav-bar/new-navbar";
-import Alert from "@/components/alert";
 
 export default function Layout({ children }) {
   return (
     <>
       <Meta />
-      <Alert />
       <div className="min-h-screen">
         <Navbar />
         <main>{children}</main>
