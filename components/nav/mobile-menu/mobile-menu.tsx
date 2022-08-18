eda84b28db8f341d50bc6884f050705247e99032
Will Kerry <will@willkerry.com>
2022-08-18 16:31:08 +0100

    Feature: add third-party link to mobile nav

--- components/nav/mobile-menu/mobile-menu.tsx
+++ components/nav/mobile-menu/mobile-menu.tsx
@@ -1,11 +1,9 @@
+import { about, aboutCTAs, memberCTAs, members, regatta } from "@/components/nav/nav-data";
 import { Popover } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
-import { about, aboutCTAs, regatta, members } from "@/components/nav/nav-data";
+
 import { regattaCTAs } from "../nav-data";
-import {
-  CompactMobileMenuSection,
-  MobileMenuSection,
-} from "./mobile-menu-section";
+import { CompactMobileMenuSection, MobileMenuSection } from "./mobile-menu-section";
 
 const MobileMenu = () => (
   <Popover.Panel
@@ -16,7 +14,10 @@ const MobileMenu = () => (
     <div className="relative bg-white divide-y rounded shadow-lg ring-1 ring-black ring-opacity-5">
       <MobileMenuSection title="About" data={about.concat(aboutCTAs)} />
       <MobileMenuSection title="Regatta" data={regatta.concat(regattaCTAs)} />
-      <CompactMobileMenuSection title="Members" data={members} />
+      <CompactMobileMenuSection
+        title="Members"
+        data={members.concat(memberCTAs)}
+      />
     </div>
     <div className="absolute top-3 right-3">
       <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-700 transition rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none">
