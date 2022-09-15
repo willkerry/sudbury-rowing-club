037196df4ff06162995c5d9b65fe4436d95c7c6b
Will Kerry <will@willkerry.com>
2022-09-15 08:12:03 +0100

    Fix: arrow-body style

--- components/stour/collapsible-card/file-group.tsx
+++ components/stour/collapsible-card/file-group.tsx
@@ -1,26 +1,24 @@
 import Link from "@/components/stour/link";
 import { type FileGroupProps } from "./collapsible-card";
 
-const FileGroup = ({ fileItems }: FileGroupProps) => {
-  return (
-    <div className="space-y-4">
-      {fileItems.map(
-        (item, i) =>
-          item.documents && (
-            <div key={i} className="flex flex-col">
-              {item.title && (
-                <h3 className="font-medium text-gray-700">{item.title}</h3>
-              )}
-              {item.documents.map((doc) => (
-                <Link key={doc._key} href={`${doc.url}?dl=`} download>
-                  {doc.title}
-                </Link>
-              ))}
-            </div>
-          )
-      )}
-    </div>
-  );
-};
+const FileGroup = ({ fileItems }: FileGroupProps) => (
+  <div className="space-y-4">
+    {fileItems.map(
+      (item, i) =>
+        item.documents && (
+          <div key={i} className="flex flex-col">
+            {item.title && (
+              <h3 className="font-medium text-gray-700">{item.title}</h3>
+            )}
+            {item.documents.map((doc) => (
+              <Link key={doc._key} href={`${doc.url}?dl=`} download>
+                {doc.title}
+              </Link>
+            ))}
+          </div>
+        )
+    )}
+  </div>
+);
 
 export default FileGroup;
