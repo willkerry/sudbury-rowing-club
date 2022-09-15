142bbf057f967989276e4e5aa7eb99185e802f20
Will Kerry <will@willkerry.com>
2022-09-15 08:59:21 +0100

    Tidy up object.

--- components/utils/date-formatter.tsx
+++ components/utils/date-formatter.tsx
@@ -1,27 +1,39 @@
 import { useState } from "react";
 
+type DateFormatPresets =
+  | "default"
+  | "long"
+  | "short"
+  | "numeric"
+  | "time"
+  | "year";
+
 type Props = {
   dateString: string | Date;
-  format?: "default" | "long" | "short" | "numeric" | "time" | "year";
+  format?: DateFormatPresets;
 };
 
-const defaultOptions: Map<string, Intl.DateTimeFormatOptions> = new Map([
-  ["default", { year: "numeric", month: "long", day: "numeric" }],
-  ["long", { year: "numeric", month: "long", day: "numeric", weekday: "long" }],
-  ["short", { year: "numeric", month: "short", day: "numeric" }],
-  ["numeric", { year: "numeric", month: "numeric", day: "numeric" }],
-  [
-    "time",
-    {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-    },
-  ],
-  ["year", { year: "numeric" }],
-]);
+const defaultOptions: Map<DateFormatPresets, Intl.DateTimeFormatOptions> =
+  new Map([
+    ["default", { year: "numeric", month: "long", day: "numeric" }],
+    [
+      "long",
+      { year: "numeric", month: "long", day: "numeric", weekday: "long" },
+    ],
+    ["short", { year: "numeric", month: "short", day: "numeric" }],
+    ["numeric", { year: "numeric", month: "numeric", day: "numeric" }],
+    [
+      "time",
+      {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      },
+    ],
+    ["year", { year: "numeric" }],
+  ]);
 
 const DateFormatter = ({ dateString, format }: Props) => {
   const [dateObj] = useState(new Date(dateString));
