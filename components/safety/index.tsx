b599ce74bd11fb1bd9f25a4548093318f6ee1186
Will Kerry <will.kerry@hbsuk.co.uk>
2022-10-09 21:21:05 +0100

    Remove commented out code

--- components/safety/index.tsx
+++ components/safety/index.tsx
@@ -1,27 +1,12 @@
 import Loading from "../stour/loading";
-import SafetyComponent, { type SafetyComponentProps } from "./safety-component";
+import SafetyComponent from "./safety-component";
 import getSafetyStatus from "@/lib/get-safety-status";
-import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const SafetyCard: React.FC = () => {
-  // const [safetyComponentProps, setSafetyComponentProps] =
-  //   useState<SafetyComponentProps>();
-
-  // useEffect(() => {
-  //   const fetchSafetyStatus = async () => {
-  //     const safetyStatus = await getSafetyStatus();
-  //     if (safetyStatus) setSafetyComponentProps(safetyStatus);
-  //   };
-  //   fetchSafetyStatus();
-  // }, []);
-
   const fetcher = () => getSafetyStatus();
 
-  const { data: safetyComponentProps, error } = useSWR(
-    "getSafetyStatus",
-    fetcher
-  );
+  const { data: safetyComponentProps } = useSWR("getSafetyStatus", fetcher);
 
   return (
     <div className="overflow-hidden border rounded">
