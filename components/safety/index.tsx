99c0afe0e9d522630366997d1104b807255b1d34
Will Kerry <will.kerry@hbsuk.co.uk>
2022-10-09 20:26:33 +0100

    Use SWR for client-side data fetching

--- components/safety/index.tsx
+++ components/safety/index.tsx
@@ -2,18 +2,26 @@ import Loading from "../stour/loading";
 import SafetyComponent, { type SafetyComponentProps } from "./safety-component";
 import getSafetyStatus from "@/lib/get-safety-status";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const SafetyCard: React.FC = () => {
-  const [safetyComponentProps, setSafetyComponentProps] =
-    useState<SafetyComponentProps>();
+  // const [safetyComponentProps, setSafetyComponentProps] =
+  //   useState<SafetyComponentProps>();
 
-  useEffect(() => {
-    const fetchSafetyStatus = async () => {
-      const safetyStatus = await getSafetyStatus();
-      if (safetyStatus) setSafetyComponentProps(safetyStatus);
-    };
-    fetchSafetyStatus();
-  }, []);
+  // useEffect(() => {
+  //   const fetchSafetyStatus = async () => {
+  //     const safetyStatus = await getSafetyStatus();
+  //     if (safetyStatus) setSafetyComponentProps(safetyStatus);
+  //   };
+  //   fetchSafetyStatus();
+  // }, []);
+
+  const fetcher = () => getSafetyStatus();
+
+  const { data: safetyComponentProps, error } = useSWR(
+    "getSafetyStatus",
+    fetcher
+  );
 
   return (
     <div className="overflow-hidden border rounded">
