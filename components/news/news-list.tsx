aad1a73c823e5057b243083e90611b88656d5279
Will Kerry <will.kerry@hbsuk.co.uk>
2022-10-18 21:03:39 +0100

    Add empty state to search results

--- components/news/news-list.tsx
+++ components/news/news-list.tsx
@@ -1,6 +1,7 @@
 import HeroPost from "./hero-post";
 import MoreStories from "./more-stories";
 import type Post from "../../types/post";
+import Link from "../stour/link";
 
 type Props = {
   posts: Post[];
@@ -8,11 +9,22 @@ type Props = {
   more?: string;
 };
 
-const NewsList = ({ posts, hero, more }: Props) => (
-  <>
-    {hero && <HeroPost post={posts[0]} />}
-    <MoreStories posts={posts.slice(hero ? 1 : 0)} {...{ more }} />
-  </>
-);
+const NewsList = ({ posts, hero, more }: Props) => {
+  if (posts.length === 0)
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-48">
+        <p className="text-lg font-semibold text-center text-gray-500">
+          Nothing found
+        </p>
+      </div>
+    );
+
+  return (
+    <>
+      {hero && <HeroPost post={posts[0]} />}
+      <MoreStories posts={posts.slice(hero ? 1 : 0)} {...{ more }} />
+    </>
+  );
+};
 
 export default NewsList;
