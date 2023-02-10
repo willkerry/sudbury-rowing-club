c5b195039f8ab1dae038fe5b0cacbfec4b5ecb2c
Will Kerry <will.kerry@hbsuk.co.uk>
2023-02-10 18:12:03 +0000

    Hack an offset scrollIntoView

--- components/landing/cta.tsx
+++ components/landing/cta.tsx
@@ -2,17 +2,26 @@ import Container from "@/components/layouts/container";
 import Button from "@/components/stour/button";
 import Link from "@/components/stour/link";
 
-const LandingCTA = () => (
-  <Container>
-    <div className="flex items-center justify-center pt-16 space-x-3 text-white">
-      <Button as="a" href="#intro" shadow size="large">
-        Discover<span className="hidden sm:inline">&nbsp;more</span>
-      </Button>
-      <Button as={Link} href="/join" variant="secondary" shadow size="large">
-        Join us
-      </Button>
-    </div>
-  </Container>
-);
+const LandingCTA = () => {
+  const scrollToIntro = () => {
+    const intro = document.getElementById("intro");
+    const introTop = intro?.getBoundingClientRect().top;
+    const offset = introTop ? introTop - 100 : 0;
+    window.scrollTo({ top: offset, behavior: "smooth" });
+  };
+
+  return (
+    <Container>
+      <div className="flex items-center justify-center pt-16 space-x-3 text-white">
+        <Button shadow size="large" onClick={scrollToIntro}>
+          Discover<span className="hidden sm:inline">&nbsp;more</span>
+        </Button>
+        <Button as={Link} href="/join" variant="secondary" shadow size="large">
+          Join us
+        </Button>
+      </div>
+    </Container>
+  );
+};
 
 export default LandingCTA;
