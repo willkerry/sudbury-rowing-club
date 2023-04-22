39206e850cfd71caa36fbea9485757d3ad711373
Will Kerry <will.kerry@hbsuk.co.uk>
2023-04-22 09:18:36 +0100

    Refactor Loading component

--- components/stour/loading/loading.tsx
+++ components/stour/loading/loading.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import styles from "./loading.module.css";
 
 type LoadingProps = {
   className?: string;
@@ -6,59 +6,27 @@ type LoadingProps = {
   visible?: boolean;
 };
 
-const Loading: React.FC<LoadingProps> = ({
-  className,
-  children,
-  visible = true,
-}) => {
-  return (
-    <div
-      className={`w-full h-full min-w-[48px] inline-flex items-center relative text-base min-h-[1em] ${className}`}
-    >
-      <div className={visible ? "hidden" : "visible" + " w-full"}>
-        {children}
-      </div>
-      <span
-        className={`absolute flex items-center content-center justify-center visible w-full h-full -translate-x-1/2 
+const Dot = () => (
+  <i
+    className={`inline-block w-1 h-1 mx-0.5 bg-gray-600 rounded-full ${styles.dots}`}
+  />
+);
+
+const Loading = ({ className, children, visible = true }: LoadingProps) => (
+  <div
+    className={`w-full h-full min-w-[48px] inline-flex items-center relative text-base min-h-[1em] ${className}`}
+  >
+    <div className={`${visible ? "hidden" : "visible"} w-full`}>{children}</div>
+    <span
+      className={`absolute flex items-center content-center justify-center visible w-full h-full -translate-x-1/2 
         -translate-y-1/2 bg-transparent select-none top-1/2 left-1/2
         ${visible ? " visible" : " hidden"}`}
-      >
-        <Dot />
-        <Dot />
-        <Dot />
-      </span>
-    </div>
-  );
-};
-
-function Dot() {
-  return (
-    <>
-      <i className="inline-block w-1 h-1 mx-0.5 bg-gray-600 rounded-full" />
-      <style jsx>{`
-        i {
-          animation: loading-blink 1.4s infinite both;
-        }
-        i:nth-child(2) {
-          animation-delay: 0.2s;
-        }
-        i:nth-child(3) {
-          animation-delay: 0.4s;
-        }
-        @keyframes loading-blink {
-          0% {
-            opacity: 0.2;
-          }
-          20% {
-            opacity: 1;
-          }
-          100% {
-            opacity: 0.2;
-          }
-        }
-      `}</style>
-    </>
-  );
-}
+    >
+      <Dot />
+      <Dot />
+      <Dot />
+    </span>
+  </div>
+);
 
 export default Loading;
