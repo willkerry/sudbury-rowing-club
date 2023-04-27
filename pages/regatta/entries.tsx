4f68d08167dd5bcb50aa372ba826bd6fbb6aa678
Will Kerry <will.kerry@hbsuk.co.uk>
2023-04-27 19:42:55 +0100

    Create fetchEntries Zod fetcher

--- pages/regatta/entries.tsx
+++ pages/regatta/entries.tsx
@@ -1,63 +1,41 @@
-import groq from "groq";
 import TextPage from "@/components/layouts/text-page";
 import EntriesComponent from "@/components/regatta/entries";
 import { CompactEvents } from "@/components/regatta/events";
 import Text from "@/components/stour/text";
 import Link from "@/components/stour/link";
-import sanityClient from "@/lib/sanity.server";
-import type { PortableTextProps } from "@portabletext/react";
-import type { Event } from "./events";
-import { NextPage } from "next";
+import type { InferGetStaticPropsType, NextPage } from "next";
+import fetchEntries from "@/lib/queries/fetch-entries";
 
-export type Entries = {
-  caption?: string;
-  description: PortableTextProps["value"];
-  waveNames: string[];
-  waves: string[][];
-};
+export const getStaticProps = async () => ({ props: await fetchEntries() });
 
-const Entries: NextPage<{
-  entries: Entries;
-  events: Event[];
-}> = ({ entries, events }) => {
+const Entries: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  entries,
+  events,
+}) => {
+  const table =
+    entries?.waves?.rows?.map((row) => row?.cells?.map((cell) => cell)) || [];
+
   return (
     <TextPage
       title="Entry Information"
       ogImage="/assets/og/entries.png"
       description="Details for competetive entry to the Sudbury Regatta."
     >
-      <CompactEvents data={events} />
+      <CompactEvents data={events.events} />
       <div className="space-x-4">
         <Link href="/regatta/events">More on events</Link>
         <Link href="/regatta/course">Course Map</Link>
       </div>
       <div className="h-16" />
       <EntriesComponent
-        table={entries?.waves}
-        caption={entries?.caption}
+        {...{ table }}
+        caption={entries?.wavesCaption}
         waveNames={entries?.waveNames}
       >
-        <Text lead portableText={entries.description} />
+        <Text lead portableText={entries.description || []} />
       </EntriesComponent>
     </TextPage>
   );
-};
-
-export const getStaticProps = async () => {
-  const data = await sanityClient.fetch(
-    groq`*[_type == "regattaSettings"][0]{entries, events}`
-  );
-  return {
-    props: {
-      entries: {
-        caption: data.entries.wavesCaption,
-        description: data.entries.description,
-        waveNames: data.entries.waveNames,
-        waves: data.entries.waves.rows.map((row: any) => row.cells),
-      },
-      events: data.events.events,
-    },
-  };
 };
 
 export default Entries;
