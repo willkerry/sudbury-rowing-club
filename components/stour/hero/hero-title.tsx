c2b4aee19d94e3a466b8fb4d677fb35e0f57b317
Will Kerry <will.kerry@hbsuk.co.uk>
2023-05-11 20:31:15 +0100

    Refactor HeroTitle's prop handling

--- components/stour/hero/hero-title.tsx
+++ components/stour/hero/hero-title.tsx
@@ -7,50 +7,95 @@ interface Props {
   children?: React.ReactNode;
   prose?: boolean;
   breadcrumbs?: boolean;
+  color?: "blue" | "gray" | "transparent";
+  /** @deprecated */
   transparent?: boolean;
 }
+
+const COLOR_MAP = new Map<
+  NonNullable<Props["color"]>,
+  {
+    container: string;
+    hero: string;
+    item: string;
+  }
+>([
+  [
+    "blue",
+    {
+      container: "bg-blue-50",
+      hero: "bg-blue-900 text-blue-50",
+      item: "text-blue-500 hover:text-blue-300",
+    },
+  ],
+  [
+    "gray",
+    {
+      container: "bg-gray-50",
+      hero: "bg-gray-900 text-gray-50",
+      item: "text-gray-500 hover:text-gray-300",
+    },
+  ],
+  [
+    "transparent",
+    {
+      container: "bg-transparent",
+      hero: "bg-transparent text-gray-900",
+      item: "text-gray-800 hover:text-gray-600",
+    },
+  ],
+]);
 
 const HeroTitle: React.FC<Props> = ({
   title,
   children,
   prose = false,
   breadcrumbs = false,
+  color = "blue",
   transparent = false,
-}) => (
-  <>
-    <div
-      className={cn(
-        "relative flex flex-wrap content-center h-48 min-h-[48]",
-        transparent
-          ? "bg-transparent text-gray-900"
-          : "bg-blue-900 text-blue-50"
-      )}
-    >
-      <Container>
-        <div className={cn(prose && "max-w-prose mx-auto text-center")}>
-          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
-            {title}
-          </h1>
-          {children}
-        </div>
-      </Container>
-    </div>
-    {breadcrumbs && (
-      <div className="py-2 bg-blue-50">
+}) => {
+  let localColor = color;
+  if (transparent) localColor = "transparent";
+
+  const colorClasses = COLOR_MAP.get(localColor);
+
+  return (
+    <>
+      <div
+        className={cn(
+          "relative flex flex-wrap content-center h-48 min-h-[48]",
+          colorClasses?.hero
+        )}
+      >
         <Container>
-          <Breadcrumbs
-            listClassName={cn(
-              "flex text-sm",
-              prose && "max-w-min mx-auto justify-center"
-            )}
-            rootLabel="Home"
-            inactiveItemClassName="whitespace-nowrap after:content-['→'] after:px-2 after:text-gray-400 capitalize text-blue-500 hover:text-blue-300 transition"
-            activeItemClassName="whitespace-nowrap font-medium capitalize"
-          />
+          <div className={cn(prose && "max-w-prose mx-auto text-center")}>
+            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+              {title}
+            </h1>
+            {children}
+          </div>
         </Container>
       </div>
-    )}
-  </>
-);
+      {breadcrumbs && (
+        <div className={cn("py-2", colorClasses?.container)}>
+          <Container>
+            <Breadcrumbs
+              listClassName={cn(
+                "flex text-sm",
+                prose && "max-w-min mx-auto justify-center"
+              )}
+              rootLabel="Home"
+              inactiveItemClassName={cn(
+                "whitespace-nowrap after:content-['→'] after:px-2 after:text-gray-400 capitalize transition",
+                colorClasses?.item
+              )}
+              activeItemClassName="whitespace-nowrap font-medium capitalize"
+            />
+          </Container>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default HeroTitle;
