c6d429565b3fed3910fdfbe7c2c516a499a2154e
Will Kerry <will.kerry@hbsuk.co.uk>
2023-05-11 15:33:17 +0100

    Add a Redis cache to speed up API

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -1,3 +1,4 @@
+import kv from "@vercel/kv";
 import { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import DOMPurify from "isomorphic-dompurify";
@@ -87,6 +88,64 @@ const sanitiseAndRename = (events: BREvent[]): SRCEvent[] =>
     }))
   );
 
+const fetchTransformAndCacheEvents = async () => {
+  const KEY = "events";
+  const TTL_SECONDS = 60 * 60 * 12; // 12 hours
+
+  const cached = await kv.get<SRCEvent[]>(KEY);
+
+  if (cached) {
+    console.log(new Date(), "Events API hit cache");
+    return cached;
+  }
+
+  const brCalendarResponse = await fetch(EVENT_CALENDAR_API);
+  const brCalendarJSON = await brCalendarResponse.json();
+
+  const validated = z.array(ZBREvent).parse(brCalendarJSON);
+  const sanitised = sanitiseAndRename(validated);
+
+  // Filter out events that occurred more than 2 weeks ago
+  const now = new Date();
+  const twoWeeksAgo = new Date(now.getTime() - 14 * 24 * 60 * 60 * 1000);
+  const filtered = sanitised.filter(
+    (event) => new Date(event.startDate) >= twoWeeksAgo
+  );
+
+  await kv.set(KEY, filtered, { ex: TTL_SECONDS });
+  console.log(new Date(), "Events cold start");
+  return filtered;
+};
+
+const dateToiCalDayFormat = (date: Date) =>
+  date.toISOString().replace(/T.*/, "").replace(/-/g, "");
+
+const createICSString = (events: SRCEvent[]) => {
+  const preamble = `BEGIN:VCALENDAR
+  VERSION:2.0
+  PRODID:-//SRC//Events//EN
+  X-WR-CALNAME:SRC Events
+  X-WR-TIMEZONE:Europe/London
+  X-WR-CALDESC:Events from the British Rowing Calendar
+  `;
+
+  const icsString = events.reduce(
+    (acc, event) =>
+      `${acc}BEGIN:VEVENT
+  UID:${event.id}
+  DTSTAMP:${new Date(event.startDate).toISOString().replace(/[-:]/g, "")}
+  DTSTART:${dateToiCalDayFormat(new Date(event.startDate))}
+  SUMMARY:${event.competition}
+  DESCRIPTION:${event.notes}
+  URL:${event.url}
+  END:VEVENT
+  `,
+    preamble
+  );
+
+  return `${icsString}END:VCALENDAR`;
+};
+
 const events = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") {
     res.status(405);
@@ -96,22 +155,18 @@ const events = async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader("Cache-Control", "s-maxage=21600");
 
   try {
-    const brCalendarResponse = await fetch(EVENT_CALENDAR_API);
-    const brCalendarJSON = await brCalendarResponse.json();
+    const events = await fetchTransformAndCacheEvents();
 
-    // Validate the response against the Zod schema (i.e. fail fast if BR change their API)
-    const validated = z.array(ZBREvent).parse(brCalendarJSON);
-    // Strip out HTML and JSify the field names
-    const sanitised = sanitiseAndRename(validated);
+    if (req.query.ical) {
+      res.setHeader("Content-Type", "text/calendar");
+      res.setHeader("Content-Disposition", "attachment; filename=events.ics");
 
-    // Filter out events that occurred more than 2 weeks ago
-    const now = new Date();
-    const twoWeeksAgo = new Date(now.getTime() - 14 * 24 * 60 * 60 * 1000);
-    const filtered = sanitised.filter(
-      (event) => new Date(event.startDate) >= twoWeeksAgo
-    );
+      res.write(createICSString(events));
+      res.end();
+      return;
+    }
 
-    res.status(200).json(filtered);
+    res.status(200).json(events);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
