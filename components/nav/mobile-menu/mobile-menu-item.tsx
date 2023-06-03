d6e582e81eea944e3032fdcb32053cab367e0f28
Will Kerry <will.kerry@hbsuk.co.uk>
2023-06-03 09:02:15 +0100

    Handle undefined icons in mobile menu

--- components/nav/mobile-menu/mobile-menu-item.tsx
+++ components/nav/mobile-menu/mobile-menu-item.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
 import { type IconNavItemType, type NavItemType } from "@/types/nav-item";
+import { Fragment } from "react";
 
-const MobileMenuItem = ({ data }: { data: IconNavItemType }) => (
-  <Link
-    key={data.name}
-    href={data.href}
-    className="flex items-center p-2 -m-2 roun dded-md hover:bg-gray-100"
-  >
-    <data.icon
-      className="flex-shrink-0 w-5 h-5 text-blue-700"
-      aria-hidden="true"
-    />
-    <span className="ml-1.5 font-medium text-gray-900">
-      {data.shortName ? data.shortName : data.name}
-    </span>
-  </Link>
-);
+const MobileMenuItem = ({ data }: { data: IconNavItemType }) => {
+  const Icon = data.icon || Fragment;
+  return (
+    <Link
+      key={data.name}
+      href={data.href}
+      className="-m-2 flex items-center rounded-md p-2 hover:bg-gray-100"
+    >
+      <Icon
+        className="h-5 w-5 flex-shrink-0 text-blue-700"
+        aria-hidden="true"
+      />
+      <span className="ml-1.5 font-medium text-gray-900">
+        {data.shortName ? data.shortName : data.name}
+      </span>
+    </Link>
+  );
+};
 
 const CompactMobileMenuItem = ({ data }: { data: NavItemType }) => (
   <Link
