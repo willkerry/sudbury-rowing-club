62292a421596fa3fe52405c9a880383128028f9a
Will Kerry <will.kerry@hbsuk.co.uk>
2023-06-04 09:53:22 +0100

    Don't return awaited promise

--- hooks/useQroqQuery.ts
+++ hooks/useQroqQuery.ts
@@ -2,6 +2,6 @@ import sanityClient from "@/lib/sanity.server";
 import useSWR from "swr";
 
 const useGroqQuery = <T>(query: string, params?: any) =>
-  useSWR(query, async () => await sanityClient.fetch<T>(query, params));
+  useSWR(query, async () => sanityClient.fetch<T>(query, params));
 
 export default useGroqQuery;
