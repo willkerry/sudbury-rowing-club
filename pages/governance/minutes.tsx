3214dd62edb13b88762090377c55ac3f3608eaa1
Will Kerry <will.kerry@hbsuk.co.uk>
2023-06-06 19:56:12 +0100

    Replace minutes page with a notice

--- pages/governance/minutes.tsx
+++ pages/governance/minutes.tsx
@@ -1,42 +1,39 @@
-import Link from "@/components/stour/link";
 import TextPage from "@/components/layouts/text-page";
-import { InferGetStaticPropsType, NextPage } from "next";
-import fetchMinutes from "@/lib/queries/fetch-minutes";
+import Note from "@/components/stour/note";
 
-export const getStaticProps = async () => {
-  const minutes = await fetchMinutes();
+const randomString = () => Math.random().toString(36).slice(2);
 
-  return { props: { minutes } };
-};
-
-const Minutes: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-  minutes,
-}) => (
+const Minutes = () => (
   <TextPage title="Minutes">
-    <table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Committee</th>
-          <th>Link</th>
-        </tr>
-      </thead>
-      <tbody>
-        {minutes.map((minute) => (
-          <tr key={minute._id} className="hover:bg-gray-50">
-            <td className="text-xs font-medium uppercase tracking-wider text-gray-600">
-              {minute.date}
-            </td>
-            <td className="text-gray-700">{minute.committee}</td>
-            <td>
-              <Link href={minute.file} download>
-                Download
-              </Link>
-            </td>
+    <div className="relative rounded">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Note label="Committee minutes are no longer published here">
+          Club members can access minutes on the myClubhouse platform. Other
+          interested parties should contact the club secretary.
+        </Note>
+      </div>
+
+      <table className="select-none blur">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Committee</th>
+            <th>Link</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {Array.from({ length: 10 }, (_, i) => i).map((minute) => (
+            <tr key={minute} className="hover:bg-gray-50">
+              <td className="text-xs font-medium uppercase tracking-wider">
+                {randomString()}
+              </td>
+              <td className="capitalize text-gray-700">{randomString()}</td>
+              <td>Download</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </TextPage>
 );
 
