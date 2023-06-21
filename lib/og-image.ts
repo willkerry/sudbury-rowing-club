cd0974175f9e01ed0cfe0baa12aaa18e984bbabf
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-06-21 21:52:59 +0100

    Suppress absolute path return

--- lib/og-image.ts
+++ lib/og-image.ts
@@ -1,6 +1,6 @@
 const BASE_PATH = "/api/og";
 
-const getBasePath = () => {
+const getAbsolutePath = () => {
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${BASE_PATH}`;
   }
@@ -8,8 +8,8 @@ const getBasePath = () => {
   return `http://localhost:3000${BASE_PATH}`;
 };
 
-export const makeShareImageURL = (title: string) => {
-  const url = new URL(getBasePath());
+export const makeShareImageURL = (title: string, absolute: boolean = false) => {
+  const url = new URL(absolute ? getAbsolutePath() : BASE_PATH);
   url.search = new URLSearchParams({ title }).toString();
 
   return url.toString();
