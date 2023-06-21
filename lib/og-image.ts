d232c4c97a762b953051d92ea796aa575983fb8d
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-06-21 22:16:06 +0100

    Use the BASE_URL constant as the authority

--- lib/og-image.ts
+++ lib/og-image.ts
@@ -1,16 +1,12 @@
+import { BASE_URL } from "@/lib/constants";
+
 const BASE_PATH = "/api/og";
 
-const getAbsolutePath = () => {
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${BASE_PATH}`;
-  }
-
-  return `http://localhost:3000${BASE_PATH}`;
-};
+const absoluteUrl = `${BASE_URL}${BASE_PATH}`;
 
 export const makeShareImageURL = (title: string, absolute: boolean = false) => {
   const searchParams = new URLSearchParams({ title }).toString();
-  const path = absolute ? getAbsolutePath() : BASE_PATH;
+  const path = absolute ? absoluteUrl : BASE_PATH;
 
   return `${path}?${searchParams}`;
 };
