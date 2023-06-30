a535dccf1f961b02bc84a4a02fe19fb6a2e0dd67
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-06-30 20:01:04 +0100

    Fix small screen rendering of author archives

--- pages/news/author/[id].tsx
+++ pages/news/author/[id].tsx
@@ -1,12 +1,12 @@
 import Container from "@/components/layouts/container";
 import Layout from "@/components/layouts/layout";
 import Label from "@/components/stour/label";
-import Link from "@/components/stour/link";
 import DateFormatter from "@/components/utils/date-formatter";
 import { makeShareImageURL } from "@/lib/og-image";
 import { fetchAuthor, fetchAllAuthors } from "@/lib/queries/fetch-authors";
 import type { GetStaticPaths, InferGetStaticPropsType, NextPage } from "next";
 import { NextSeo } from "next-seo";
+import Link from "next/link";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await fetchAllAuthors();
@@ -60,12 +60,12 @@ export const AuthorArchive: NextPage<
         </p>
       </Container>
     </div>
-    <Container>
-      <ul className="my-8">
+    <Container className="prose my-6">
+      <ul className="pl-0">
         {author?.articles?.map(({ _id, slug, title, date }) => (
-          <li key={_id} className="mb-2 grid">
+          <li key={_id} className="list-none pl-0 leading-tight">
             <Link href={`/news/${slug}`}>{title}</Link>
-            <Label className="text-xs">
+            <Label className="mt-1 block text-xs">
               <DateFormatter dateString={date} format="short" />
             </Label>
           </li>
