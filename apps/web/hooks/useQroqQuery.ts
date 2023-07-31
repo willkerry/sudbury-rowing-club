68c8ccd254384e77c6d1b082b278591e22e307f0
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-07-31 10:03:38 +0100

    Configure SWR to refetch less

--- apps/web/hooks/useQroqQuery.ts
+++ apps/web/hooks/useQroqQuery.ts
@@ -2,6 +2,12 @@ import sanityClient from "@/lib/sanity.server";
 import useSWR from "swr";
 
 const useGroqQuery = <T>(query: string, params?: any) =>
-  useSWR(query, async () => sanityClient.fetch<T>(query, params));
+  useSWR(query, async () => sanityClient.fetch<T>(query, params), {
+    revalidateIfStale: false,
+    revalidateOnMount: false,
+    revalidateOnReconnect: false,
+    revalidateOnFocus: false,
+    dedupingInterval: 5 * 60 * 1000,
+  });
 
 export default useGroqQuery;
