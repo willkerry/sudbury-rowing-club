faa0304b55ca79947d0eaf54c2e0ed5291c6391c
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-07-31 10:06:49 +0100

    Refetch when component mounts first time

--- apps/web/hooks/useQroqQuery.ts
+++ apps/web/hooks/useQroqQuery.ts
@@ -4,7 +4,6 @@ import useSWR from "swr";
 const useGroqQuery = <T>(query: string, params?: any) =>
   useSWR(query, async () => sanityClient.fetch<T>(query, params), {
     revalidateIfStale: false,
-    revalidateOnMount: false,
     revalidateOnReconnect: false,
     revalidateOnFocus: false,
     dedupingInterval: 5 * 60 * 1000,
