f36b48c18d439939307ffbc2e98cdacd509e23ba
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-10-09 20:14:40 +0100

    Add Plausible script

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { DefaultSeo } from "next-seo";
 import "../styles/index.css";
 import { AppProps } from "next/app";
@@ -8,6 +9,12 @@ import "@fontsource/jetbrains-mono/variable.css";
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <DefaultSeo {...SEO} />
+    <Script
+      defer
+      data-domain="sudburyrowingclub.org.uk"
+      src="https://analytics.sudburyrowingclub.org.uk/js/script.js"
+    />
+
     <Component {...pageProps} />
   </>
 );
