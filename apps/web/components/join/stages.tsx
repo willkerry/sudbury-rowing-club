1f98f4bc9de61e83601af10f9ad9093bfb52cd07
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-11-02 17:10:56 +0000

    Migrate Stages component styles to CSS Modules

--- apps/web/components/join/stages.tsx
+++ apps/web/components/join/stages.tsx
@@ -1,4 +1,5 @@
 import Label from "@/components/stour/label";
+import styles from "./stages.module.css";
 
 type Stage = { name: string; content: string };
 type Props = {
@@ -10,35 +11,9 @@ const L2RStages = ({ stages }: Props) => (
     <Label as="h3" className="mb-6 ml-8 text-green-600 md:ml-0">
       How it works
     </Label>
-    <ol className="grid gap-16 text-green-500 sm:grid-cols-2 md:grid-cols-3">
-      <style jsx>{`
-        ol {
-          list-style: none;
-          counter-reset: a;
-        }
-        ol li {
-          counter-increment: a;
-          position: relative;
-        }
-        ol li::before {
-          content: counter(a);
-          font-weight: 500;
-          font-size: 0.875rem;
-          text-align: center;
-          position: absolute;
-          border-radius: 100%;
-          border-width: 1px;
-          border-color: currentColor;
-          width: 1.25rem;
-          height: 1.25rem;
-          top: 1px;
-          left: -2rem;
-          line-height: 1.3;
-        }
-        ol li p {
-          color: inherit;
-        }
-      `}</style>
+    <ol
+      className={`grid gap-16 text-green-500 sm:grid-cols-2 md:grid-cols-3 ${styles.stages}`}
+    >
       {stages.map((stage) => (
         <li key={stage.name} className="ml-8 lg:ml-0">
           <div className="text-lg font-semibold leading-tight tracking-tight text-gray-700">
