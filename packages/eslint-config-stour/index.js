d66eff58aad2137c9e8460cc14337fcf53f3b27b
Will Kerry <24854452+willkerry@users.noreply.github.com>
2023-11-02 20:39:54 +0000

    Rule `jsx-a11y/control-has-associated-label` is naïvely implemented

--- packages/eslint-config-stour/index.js
+++ packages/eslint-config-stour/index.js
@@ -30,6 +30,7 @@ module.exports = {
     "sort-keys-fix/sort-keys-fix": 0, // switch on in order to enable auto-sort of keys
     "react/react-in-jsx-scope": 0,
     "jsx-a11y/anchor-is-valid": 0,
+    "jsx-a11y/control-has-associated-label": 0,
     "import/extensions": 0,
     "import/no-unresolved": 1,
     "react/jsx-filename-extension": [
