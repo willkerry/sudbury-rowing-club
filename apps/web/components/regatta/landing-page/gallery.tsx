37fb7b39dd283e3af07dbca31b9b011f84fb8998
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-01-26 18:00:18 +0000

    Use CDN image in regatta gallery

--- apps/web/components/regatta/landing-page/gallery.tsx
+++ apps/web/components/regatta/landing-page/gallery.tsx
@@ -1,5 +1,5 @@
+import { useSanityImageProps } from "@/hooks/useSanityImageProps";
 import Image from "next/image";
-import { urlFor } from "@sudburyrc/api";
 
 type ImageType = {
   _id: string;
@@ -8,33 +8,35 @@ type ImageType = {
   aspectRatio: number;
 };
 
+const GalleryFigure = ({ _id, caption, lqip, aspectRatio }: ImageType) => (
+  <figure
+    className="relative flex-none shrink-0 snap-center overflow-hidden first:pl-8 last:pr-8"
+    style={{ width: 200 * aspectRatio }}
+  >
+    <div className="relative flex overflow-hidden rounded">
+      <Image
+        {...useSanityImageProps(_id)}
+        width={200 * aspectRatio}
+        height={200}
+        quality={30}
+        placeholder="blur"
+        blurDataURL={lqip}
+        alt={caption || ""}
+      />
+    </div>
+    <figcaption className="mt-1 flex items-center text-sm text-gray-600">
+      {caption}
+    </figcaption>
+  </figure>
+);
+
 const Gallery = ({ images }: { images: ImageType[] }) => (
   <div className="relative flex w-full snap-x gap-6 overflow-x-auto pb-14">
     <div className="shrink-0 snap-center">
       <div className="w-4 shrink-0 sm:w-48" />
     </div>
     {images.map((image) => (
-      <figure
-        key={image._id}
-        className="relative flex-none shrink-0 snap-center overflow-hidden first:pl-8 last:pr-8"
-        style={{
-          width: 200 * image.aspectRatio,
-        }}
-      >
-        <div className="relative flex overflow-hidden rounded">
-          <Image
-            src={urlFor(image._id).height(400).url()}
-            width={200 * image.aspectRatio}
-            height={200}
-            placeholder="blur"
-            blurDataURL={image.lqip}
-            alt={image.caption || ""}
-          />
-        </div>
-        <figcaption className="mt-1 flex items-center text-sm text-gray-600">
-          {image.caption}
-        </figcaption>
-      </figure>
+      <GalleryFigure {...image} key={image._id} />
     ))}
     <div className="shrink-0 snap-center">
       <div className="w-4 shrink-0 sm:w-48" />
