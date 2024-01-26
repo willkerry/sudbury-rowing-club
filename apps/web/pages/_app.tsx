0d8a76be3f8c5e9d8d5079423b44796c9c5b4382
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-01-26 20:37:50 +0000

    Add React Query devtools

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Script from "next/script";
+import dynamic from "next/dynamic";
 import { DefaultSeo } from "next-seo";
 import "../styles/index.css";
 import { AppProps } from "next/app";
@@ -7,17 +8,31 @@ import SEO from "../next-seo.config";
 import "inter-ui/inter-variable.css";
 import "@fontsource-variable/jetbrains-mono/index.css";
 
+const ReactQueryDevtools = dynamic(
+  () =>
+    import("@tanstack/react-query-devtools").then(
+      (mod) => mod.ReactQueryDevtools,
+    ),
+  { ssr: false },
+);
+
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => (
   <QueryClientProvider client={queryClient}>
     <DefaultSeo {...SEO} />
+
     <Script
       defer
       data-domain="sudburyrowingclub.org.uk"
       src="https://analytics.sudburyrowingclub.org.uk/js/script.js"
     />
+
     <Component {...pageProps} />
+
+    {process.env.NODE_ENV === "development" && (
+      <ReactQueryDevtools initialIsOpen={false} />
+    )}
   </QueryClientProvider>
 );
 
