23d59ffea3b65019c87d08953f9aae3f46876683
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-03-03 16:19:39 +0000

    Added hover effects to wave matrix

--- apps/web/components/regatta/entries.tsx
+++ apps/web/components/regatta/entries.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import getBoatsByWave from "../utils/getBoatsByWave";
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
   table: string[][];
   waveNames: string[];
   caption?: string;
+  compact?: boolean;
 };
 
 const WAVE_COLORS = [
@@ -16,40 +18,122 @@ const WAVE_COLORS = [
   "bg-pink-500 text-white",
 ];
 
-const Entries = ({ children, table, waveNames, caption }: Props) => {
+const BOAT_CLASS_SYMBOLS: Record<string, string> = {
+  "×": "scull",
+  "+": "coxed",
+  "−": "coxless",
+  "/": "or",
+  " ": "-",
+};
+
+const sanitizeBoatClass = (boatClass: string) =>
+  boatClass
+    .split("")
+    .map((char) => BOAT_CLASS_SYMBOLS[char] || char)
+    .join("")
+    .concat("_only");
+
+const getElementsByBoatName = (boatName: string) =>
+  document.querySelectorAll(`[data-boatid^="${boatName}"]`);
+
+const highlightBoat = (boatName: string) => {
+  const boatElements = getElementsByBoatName(boatName);
+
+  boatElements.forEach((boatElement) => {
+    boatElement.classList.add("ring-2");
+  });
+};
+
+const unhighlightBoat = (boatName: string) => {
+  const boatElements = getElementsByBoatName(boatName);
+
+  boatElements.forEach((boatElement) => {
+    boatElement.classList.remove("ring-2");
+  });
+};
+
+const Chip = ({
+  children,
+  color,
+  id,
+  className,
+}: {
+  children: React.ReactNode;
+  location: "table" | "list";
+  color: string;
+  id: string;
+  className?: string;
+}) => {
+  const sanitisedId = sanitizeBoatClass(id);
+
+  return (
+    <button
+      data-boatid={sanitisedId}
+      type="button"
+      className={cn(
+        "rounded px-0.5 font-medium ring-black ring-offset-1 transition",
+        color,
+        className,
+      )}
+      onMouseEnter={() => highlightBoat(sanitisedId)}
+      onMouseLeave={() => unhighlightBoat(sanitisedId)}
+      onFocus={() => highlightBoat(sanitisedId)}
+      onBlur={() => unhighlightBoat(sanitisedId)}
+      onClick={() => highlightBoat(sanitisedId)}
+    >
+      {children}
+    </button>
+  );
+};
+
+const Entries = ({ children, table, waveNames, caption, compact }: Props) => {
   const boatsByWave = getBoatsByWave(table, "Any");
   const getWaveColor = (entry: string) =>
     WAVE_COLORS[waveNames.indexOf(entry)] || "bg-gray-100";
+
+  const indicesToBoatName = (categoryIndex: number, boatClassIndex: number) =>
+    `${table[categoryIndex + 1][0]} ${table[0][boatClassIndex + 1]}`;
+
   return (
     <div className="mx-auto">
       {children}
-      <figure className="prose my-12 text-xs sm:text-sm lg:text-base">
+      <figure
+        className={cn(
+          "prose",
+          "overflow-x-auto",
+          compact ? "my-6 prose-sm" : "my-12 text-xs sm:text-sm lg:text-base",
+        )}
+      >
         <table>
           <thead>
             <tr>
               {table[0].map((entry) => (
-                <th key={entry} className="text-center">
+                <th key={entry} className="text-center px-pt sm:px-1">
                   {entry}
                 </th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {table.slice(1).map((wave) => (
+            {table.slice(1).map((wave, category) => (
               <tr key={wave[0]}>
                 <th>{wave[0]}</th>
-                {wave.slice(1).map((entry) => (
-                  <td key={entry} className="text-center">
-                    <div
-                      className={`rounded-lg px-0.5 font-medium ${getWaveColor(
-                        entry,
-                      )}
-                      `}
-                    >
-                      {entry}
-                    </div>
-                  </td>
-                ))}
+                {wave.slice(1).map((entry, boatClass) => {
+                  if (!entry) return <td />;
+
+                  return (
+                    <td key={entry} className="text-center px-0.5 sm:px-1">
+                      <Chip
+                        id={indicesToBoatName(category, boatClass)}
+                        location="table"
+                        className="w-full"
+                        color={getWaveColor(entry)}
+                      >
+                        {entry}
+                      </Chip>
+                    </td>
+                  );
+                })}
               </tr>
             ))}
           </tbody>
@@ -58,15 +142,27 @@ const Entries = ({ children, table, waveNames, caption }: Props) => {
       </figure>
 
       {Object.keys(boatsByWave).map((wave) => (
-        <div className="text-sm" key={wave}>
-          <h4 className="inline-block pr-2">Wave {wave}: </h4>
+        <div className="prose prose-sm text-sm" key={wave}>
+          <h4 className="inline-block mb-0 pr-2">Wave {wave}: </h4>
+
           {boatsByWave[wave].map((boat, i) => {
             const full = `${boat.x} ${boat.y}`;
+            const isLast = i === boatsByWave[wave].length - 1;
+
             return (
-              <span key={boat.x} className="inline-block pr-2">
-                {full}
-                {i < boatsByWave[wave].length - 1 && ", "}
-              </span>
+              <>
+                <Chip
+                  id={full}
+                  location="list"
+                  color={getWaveColor(wave)}
+                  key={full}
+                  className={cn("text-xs px-1", isLast && "mb-2")}
+                >
+                  {full}
+                </Chip>
+
+                {!isLast && ", "}
+              </>
             );
           })}
         </div>
