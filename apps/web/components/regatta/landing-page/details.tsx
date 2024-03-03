733ad6b1b49550d6527d15a5dedc1a44aeb4ac3e
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-03-03 22:11:00 +0000

    Add scroll hint UI to regatta accordion

--- apps/web/components/regatta/landing-page/details.tsx
+++ apps/web/components/regatta/landing-page/details.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { Mouse } from "lucide-react";
+import { cn } from "@/lib/utils";
 import {
   Accordion,
   AccordionContent,
@@ -12,24 +15,50 @@ export type DetailProps = {
   children: React.ReactNode;
 };
 
-const Details = ({ items }: { items: DetailProps[] }) => (
-  <Accordion type="single" collapsible>
-    {items.map(({ summary, icon, children }) => (
-      <AccordionItem key={summary} value={summary}>
-        <AccordionTrigger>
-          <div className="flex items-center justify-between gap-x-2">
-            <Slot aria-hidden className="h-6 w-6 stroke-[1.5px] text-gray-400">
-              {icon}
-            </Slot>
-            {summary}
-          </div>
-        </AccordionTrigger>
-        <AccordionContent className="h-96 border-t pt-2 overflow-y-auto resize-y">
-          {children}
-        </AccordionContent>
-      </AccordionItem>
-    ))}
-  </Accordion>
-);
+const Details = ({ items }: { items: DetailProps[] }) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  return (
+    <Accordion type="single" collapsible>
+      {items.map(({ summary, icon, children }) => (
+        <AccordionItem key={summary} value={summary}>
+          <AccordionTrigger>
+            <div className="flex items-center justify-between gap-x-2">
+              <Slot
+                aria-hidden
+                className="h-6 w-6 stroke-[1.5px] text-gray-400"
+              >
+                {icon}
+              </Slot>
+              {summary}
+            </div>
+          </AccordionTrigger>
+          <AccordionContent
+            className="relative h-96 resize-y overflow-y-auto border-t pt-2"
+            onScrollCapture={() => {
+              if (scrolled) return;
+
+              setScrolled(true);
+            }}
+          >
+            {children}
+
+            <div
+              aria-hidden
+              className="pointer-events-none absolute bottom-1 left-0 right-0"
+            >
+              <Mouse
+                className={cn(
+                  "mx-auto h-6 w-6 animate-bounce fill-white stroke-[1.5px] text-gray-400 opacity-100 transition-opacity",
+                  scrolled && "opacity-0",
+                )}
+              />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
 
 export default Details;
