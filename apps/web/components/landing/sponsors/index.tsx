0481a1c15b3e13e28440e2f197c92001c62ced28
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-04-24 16:18:53 +0100

    Add new sponsor logos

--- apps/web/components/landing/sponsors/index.tsx
+++ apps/web/components/landing/sponsors/index.tsx
@@ -1,48 +1,99 @@
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 import BritishRowing from "@/components/landing/sponsors/british-rowing";
 import Errc from "@/components/landing/sponsors/errc";
 import Rowperfect from "@/components/landing/sponsors/rowperfect";
-import Specflue from "@/components/landing/sponsors/specflue";
+import ABGlass from "./ab-glass";
+import Ashtons from "./ashtons";
+import MooreGreen from "./moore-green";
+import Swan from "./swan";
 
 interface SponsorLogo {
   logo: React.ReactElement;
   href: string;
   name: string;
+  type: "sponsor" | "affiliate";
 }
 
-const SponsorLogos: SponsorLogo[] = [
+const logos: SponsorLogo[] = [
   {
-    logo: <Specflue aria-hidden className="h-7" />,
-    href: "https://specflue.com/",
-    name: "Specflue",
-  },
-  {
-    logo: <Rowperfect aria-hidden className="h-4" />,
+    logo: <Rowperfect aria-hidden className="h-3 lg:h-4" />,
     href: "https://www.rowperfect.co.uk/",
     name: "Rowperfect",
+    type: "sponsor",
+  },
+  {
+    logo: <ABGlass aria-hidden className="h-8 pb-1.5 lg:h-9" />,
+    href: "https://www.abglasshomeimprovements.co.uk/",
+    name: "A&B Glass Home Improvements",
+    type: "sponsor",
+  },
+  {
+    logo: <MooreGreen aria-hidden className="h-4 lg:h-5" />,
+    href: "https://www.mooregreen.co.uk/",
+    name: "Moore Green",
+    type: "sponsor",
+  },
+  {
+    logo: <Ashtons aria-hidden className="h-7 lg:h-8" />,
+    href: "https://www.ashtonslegal.co.uk/",
+    name: "Ashtons",
+    type: "sponsor",
+  },
+  {
+    logo: <Swan aria-hidden className="h-9 pb-1 lg:h-11" />,
+    href: "https://theswanatlavenham.co.uk/?utm_source=website&utm_medium=listing&utm_campaign=sudbury_rowing_club",
+    name: "The Swan at Lavenham",
+    type: "sponsor",
   },
   {
     logo: <BritishRowing aria-hidden className="h-8" />,
     href: "https://britishrowing.org/",
     name: "British Rowing",
+    type: "affiliate",
   },
   {
     logo: <Errc aria-hidden className="h-7" />,
     href: "http://easternregionrowing.org.uk/",
     name: "Eastern Region Rowing Council",
+    type: "affiliate",
   },
 ];
+const LogoListItem = ({ logo, href, name }: SponsorLogo) => (
+  <li key={`${href}${name}`}>
+    <Link href={href} aria-label={name}>
+      {logo}
+    </Link>
+  </li>
+);
 
-const Sponsors = () => (
-  <ul className="space-between my-12 flex flex-wrap items-center justify-between gap-4">
-    {SponsorLogos.map(({ logo, href, name }) => (
-      <li key={`${href}${name}`}>
-        <Link href={href} aria-label={name}>
-          {logo}
-        </Link>
-      </li>
+const LogoList = ({
+  logos,
+  className,
+}: {
+  logos: SponsorLogo[];
+  className?: string;
+}) => (
+  <ul
+    className={cn(
+      "space-between my-12 flex flex-wrap items-center justify-center gap-10 md:justify-between md:gap-4",
+      className,
+    )}
+  >
+    {logos.map((logo) => (
+      <LogoListItem key={logo.name} {...logo} />
     ))}
   </ul>
+);
+
+const sponsorLogos = logos.filter(({ type }) => type === "sponsor");
+const affiliateLogos = logos.filter(({ type }) => type === "affiliate");
+
+const Sponsors = () => (
+  <>
+    <LogoList logos={sponsorLogos} />
+    <LogoList logos={affiliateLogos} className="md:justify-center md:gap-8" />
+  </>
 );
 
 export default Sponsors;
