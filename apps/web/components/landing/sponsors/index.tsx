5020b87640997306562ca516539ce54c65aae1a6
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-04-25 21:09:59 +0100

    Use static SVG files for sponsor logos

--- apps/web/components/landing/sponsors/index.tsx
+++ apps/web/components/landing/sponsors/index.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { shuffle } from "radash";
 import { cn } from "@/lib/utils";
-import BritishRowing from "@/components/landing/sponsors/british-rowing";
-import Errc from "@/components/landing/sponsors/errc";
-import Rowperfect from "@/components/landing/sponsors/rowperfect";
-import ABGlass from "./ab-glass";
-import Ashtons from "./ashtons";
-import MooreGreen from "./moore-green";
-import Swan from "./swan";
+import abglass from "../../../public/assets/logos/ab-glass-home-improvement.svg";
+import ashtons from "../../../public/assets/logos/ashtons.svg";
+import britishRowing from "../../../public/assets/logos/british-rowing.svg";
+import errc from "../../../public/assets/logos/errc.svg";
+import mooreGreen from "../../../public/assets/logos/moore-green.svg";
+import rowperfect from "../../../public/assets/logos/rowperfect.svg";
+import swan from "../../../public/assets/logos/swan-at-lavenham.svg";
 
 interface SponsorLogo {
-  logo: React.ReactElement;
+  logo: any;
   href: string;
   name: string;
   type: "sponsor" | "affiliate";
@@ -18,43 +19,87 @@ interface SponsorLogo {
 
 const logos: SponsorLogo[] = [
   {
-    logo: <Rowperfect aria-hidden className="h-3 lg:h-4" />,
+    logo: (
+      <Image
+        src={rowperfect}
+        alt=""
+        aria-hidden
+        className="h-3 w-auto select-none lg:h-4"
+      />
+    ),
     href: "https://www.rowperfect.co.uk/",
     name: "Rowperfect",
     type: "sponsor",
   },
   {
-    logo: <ABGlass aria-hidden className="h-8 pb-1.5 lg:h-9" />,
+    logo: (
+      <Image
+        src={abglass}
+        alt=""
+        aria-hidden
+        className="h-8 w-auto select-none pb-1.5 lg:h-9"
+      />
+    ),
     href: "https://www.abglasshomeimprovements.co.uk/",
     name: "A&B Glass Home Improvements",
     type: "sponsor",
   },
   {
-    logo: <MooreGreen aria-hidden className="h-4 lg:h-5" />,
+    logo: (
+      <Image
+        src={mooreGreen}
+        alt=""
+        aria-hidden
+        className="h-4 w-auto select-none lg:h-5"
+      />
+    ),
     href: "https://www.mooregreen.co.uk/",
     name: "Moore Green",
     type: "sponsor",
   },
   {
-    logo: <Ashtons aria-hidden className="h-7 lg:h-8" />,
+    logo: (
+      <Image
+        src={ashtons}
+        alt=""
+        aria-hidden
+        className="h-7 w-auto select-none lg:h-8"
+      />
+    ),
     href: "https://www.ashtonslegal.co.uk/",
     name: "Ashtons",
     type: "sponsor",
   },
   {
-    logo: <Swan aria-hidden className="h-9 pb-1 lg:h-11" />,
+    logo: (
+      <Image
+        src={swan}
+        alt=""
+        aria-hidden
+        className="h-9 w-auto select-none pb-1 lg:h-11"
+      />
+    ),
     href: "https://theswanatlavenham.co.uk/?utm_source=website&utm_medium=listing&utm_campaign=sudbury_rowing_club",
     name: "The Swan at Lavenham",
     type: "sponsor",
   },
   {
-    logo: <BritishRowing aria-hidden className="h-8" />,
+    logo: (
+      <Image
+        src={britishRowing}
+        alt=""
+        aria-hidden
+        className="h-8 w-auto select-none lg:h-4"
+      />
+    ),
     href: "https://britishrowing.org/",
     name: "British Rowing",
     type: "affiliate",
   },
   {
-    logo: <Errc aria-hidden className="h-7" />,
+    logo: (
+      <Image src={errc} alt="" aria-hidden className="h-7 w-auto select-none" />
+    ),
     href: "http://easternregionrowing.org.uk/",
     name: "Eastern Region Rowing Council",
     type: "affiliate",
