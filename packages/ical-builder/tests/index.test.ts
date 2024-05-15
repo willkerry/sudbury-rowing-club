d1cd8e6298d7f870430a848576a6374dd88f43d3
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-05-15 20:39:07 +0100

    Add IcalBuilder testing

---
+++ packages/ical-builder/tests/index.test.ts
@@ -0,0 +1,43 @@
+import { SRCEvent } from "@sudburyrc/api";
+import { fakerEN_GB as faker } from "@faker-js/faker";
+import { expect, test } from "vitest";
+import IcalBuilder from "../src/index";
+
+const NUMBER_OF_EVENTS = 10;
+const FIRST_DATE = "2024-05-01";
+
+const addNWeeksToDate = (n: number): string => {
+  const d = new Date(FIRST_DATE);
+  d.setTime(d.getTime() + n * 7 * 24 * 60 * 60 * 1000);
+
+  return d.toISOString();
+};
+
+faker.seed(0);
+
+const events: SRCEvent[] = new Array(NUMBER_OF_EVENTS)
+  .fill(null)
+  .map((_, i) => {
+    const date = addNWeeksToDate(i);
+
+    return {
+      id: faker.commerce.isbn(),
+      startDate: date,
+      endDate: date,
+      competition: faker.location.city() + " Regatta",
+      region: faker.location.county(),
+      notes: faker.lorem.sentence(),
+      url: faker.internet.url(),
+      status: faker.datatype.boolean() ? 0 : 1,
+    };
+  });
+
+test("IcalBuilder", () => {
+  const builder = new IcalBuilder("SudburyRC", "Europe/London", "SudburyRC");
+
+  builder.set(events);
+
+  const ical = builder.stringify();
+
+  expect(ical).toMatchSnapshot();
+});
