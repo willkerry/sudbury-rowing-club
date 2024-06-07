7b7632106fc60f08516f988276f22552a0fbdd09
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-06-07 14:08:40 +0100

    Use Node runtime

--- apps/web/app/safety/(landing)/@status/page.tsx
+++ apps/web/app/safety/(landing)/@status/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SafetyComponent from "@/components/safety/safety-component";
 
-export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 const Safety = async () => <SafetyComponent />;
