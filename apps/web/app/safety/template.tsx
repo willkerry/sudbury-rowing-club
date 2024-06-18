522a43add6345d3ca0505de4b67c8842e971a623
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-06-18 19:43:17 +0100

    Remove page transition

--- apps/web/app/safety/template.tsx
+++
@@ -1,9 +0,0 @@
-import { PageTransitionEffect } from "@/components/utils/page-transition";
-
-export default function rootTemplate({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <PageTransitionEffect>{children}</PageTransitionEffect>;
-}
