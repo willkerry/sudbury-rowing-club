b7ff5217546a1e9c70f190aef36fd61d1e939d12
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-07-22 20:57:59 +0100

    Enable code splitting for @sudburyrc/blue

--- packages/blue/tsup.config.ts
+++ packages/blue/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/blue.ts", "src/crest.tsx", "src/wordmark.tsx"],
   dts: true,
   sourcemap: true,
-  splitting: false,
+  splitting: true,
   clean: true,
   minify: true,
   minifyWhitespace: true,
