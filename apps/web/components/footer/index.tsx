c8cb8832d8bda81fd47232b49d258ebfbbe84627
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-09-14 19:25:22 +0100

    Refactor footer

--- apps/web/components/footer/index.tsx
+++ apps/web/components/footer/index.tsx
@@ -13,38 +13,44 @@ import Address from "./address";
 import FooterColumn from "./column";
 import ReportABug from "./reportABug";
 import SocialIcons from "./socialIcons";
+import type { IconNavItemType } from "@/types/nav-item";
 
-const communityFooter = [...members.filter((item) => item.cta), ...socials];
+const columns: [string, IconNavItemType[]][] = [
+  ["About", about],
+  ["Regatta", regatta],
+  ["Community", [...members.filter((item) => item.cta), ...socials]],
+  ["Miscellany", misc],
+];
 
-export default function Footer() {
-  const date = new Date();
-  return (
-    <footer className="border-t">
-      <Container>
-        <h2 className="sr-only">Footer</h2>
-        <div className="justify-left flex flex-wrap py-16">
-          <div className="w-1/2 sm:w-4/12 md:w-3/12">
-            <Crest aria-hidden className="mb-6 h-12 text-gray-600 md:h-16" />
-            <Address />
+const Footer = () => (
+  <footer className="border-t">
+    <Container>
+      <h2 className="sr-only">Footer</h2>
+      <div className="justify-left flex flex-wrap py-16">
+        <div className="w-1/2 sm:w-4/12 md:w-3/12">
+          <Crest aria-hidden className="mb-6 h-12 text-gray-600 md:h-16" />
+          <Address />
+        </div>
+      </div>
+
+      <div className="justify-left mb-12 flex flex-wrap">
+        {columns.map(([heading, data]) => (
+          <FooterColumn key={heading} heading={heading} data={data} />
+        ))}
+      </div>
+
+      <div className="pt-2">
+        <div className="m-auto flex justify-between py-5 text-sm text-gray-500 md:flex-row">
+          <div className="mt-2">
+            © Sudbury Rowing Club {new Date().getFullYear()}. <ReportABug />
+          </div>
+          <div className="mt-2 flex flex-row">
+            <SocialIcons />
           </div>
         </div>
-        <div className="justify-left mb-12 flex flex-wrap">
-          <FooterColumn heading="About" data={about} />
-          <FooterColumn heading="Regatta" data={regatta} />
-          <FooterColumn heading="Community" data={communityFooter} />
-          <FooterColumn heading="Miscellany" data={misc} />
-        </div>
-        <div className="pt-2">
-          <div className="m-auto flex justify-between py-5 text-sm text-gray-500 md:flex-row">
-            <div className="mt-2">
-              © Sudbury Rowing Club {date.getFullYear()}. <ReportABug />
-            </div>
-            <div className="mt-2 flex flex-row">
-              <SocialIcons />
-            </div>
-          </div>
-        </div>
-      </Container>
-    </footer>
-  );
-}
+      </div>
+    </Container>
+  </footer>
+);
+
+export default Footer;
