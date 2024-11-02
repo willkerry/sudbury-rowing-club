f80c043e177e0776cbe5122647d57776c99c56a5
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-11-02 08:33:17 +0000

    Add explicit return type to `getSanityConfig()`

--- packages/api/src/sanity/client.ts
+++ packages/api/src/sanity/client.ts
@@ -1,9 +1,9 @@
-import { createClient } from "next-sanity";
+import { createClient, type ClientConfig } from "next-sanity";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
-export const getSanityConfig = () => {
+export const getSanityConfig = (): ClientConfig => {
   if (!projectId) throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
   if (!dataset) throw new Error("Missing NEXT_PUBLIC_SANITY_DATASET");
 
