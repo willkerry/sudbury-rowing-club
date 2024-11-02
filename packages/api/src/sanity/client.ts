2ef0fda19a166d60c643325e093ad8ea6d4abc08
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-11-02 11:56:48 +0000

    Use `satisfies` to validate getSanityConfig return type

--- packages/api/src/sanity/client.ts
+++ packages/api/src/sanity/client.ts
@@ -3,16 +3,16 @@ import { createClient, type ClientConfig } from "next-sanity";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
-export const getSanityConfig = (): ClientConfig => {
+export const getSanityConfig = () => {
   if (!projectId) throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
   if (!dataset) throw new Error("Missing NEXT_PUBLIC_SANITY_DATASET");
 
   return {
     dataset: dataset || "production",
-    projectId,
+    projectId: projectId,
     apiVersion: "2021-03-25",
     useCdn: true,
-  };
+  } satisfies ClientConfig;
 };
 
 export const sanityClient = createClient(getSanityConfig());
