fcd328444ca392e7a35214231debbc84ebf71659
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-11-12 12:46:39 +0000

    Use cva for HeroTitle styles

--- apps/web/components/stour/hero/hero-title.tsx
+++ apps/web/components/stour/hero/hero-title.tsx
@@ -1,54 +1,126 @@
-import cn from "clsx";
 import Container from "../../layouts/container";
 import Breadcrumbs from "../breadcrumbs";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface Props {
+interface BaseProps {
   title: string;
   children?: React.ReactNode;
-  prose?: boolean;
   breadcrumbs?: boolean;
-  color?: "blue" | "gray" | "transparent";
   /** @deprecated */
   transparent?: boolean;
 }
 
-const COLOR_MAP = new Map<
-  NonNullable<Props["color"]>,
+const ROOT_LABEL = "Home";
+
+const headingVariants = cva("text-4xl sm:text-5xl md:text-6xl", {
+  variants: {
+    color: {
+      blue: "font-bold",
+      gray: "font-semibold",
+      transparent: "font-semibold",
+    },
+    prose: {
+      true: "mx-auto max-w-prose text-center",
+    },
+  },
+});
+
+const heroVariants = cva(
+  "relative flex h-24 flex-wrap content-center sm:h-36 md:h-48",
   {
-    container: string;
-    heading: string;
-    hero: string;
-    item: string;
-  }
->([
-  [
-    "blue",
+    variants: {
+      color: {
+        blue: "bg-blue-950 text-blue-50",
+        gray: "bg-gray-900 text-gray-50",
+        transparent: "bg-transparent text-gray-900",
+      },
+    },
+  },
+);
+
+const HeroTitleSubcomponent = ({
+  color,
+  title,
+  children,
+  prose,
+}: BaseProps & VariantProps<typeof headingVariants>) => (
+  <div className={heroVariants({ color })}>
+    <Container>
+      <div className={headingVariants({ color, prose })}>
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </Container>
+  </div>
+);
+
+const breadcrumbContainerVariants = cva("py-2", {
+  variants: {
+    color: {
+      blue: "bg-blue-50",
+      gray: "border-b bg-gray-100",
+      transparent: "bg-gray-50",
+    },
+    prose: { true: "", false: "" },
+  },
+  compoundVariants: [
     {
-      container: "bg-blue-50",
-      heading: "font-bold",
-      hero: "bg-blue-950 text-blue-50",
-      item: "text-blue-500 hover:text-blue-300",
+      color: "transparent",
+      prose: true,
+      className: "mx-auto w-min rounded-full border",
+    },
+    {
+      color: "transparent",
+      prose: false,
+      className: "border-y",
     },
   ],
-  [
-    "gray",
-    {
-      container: "border-b bg-gray-100",
-      heading: "font-semibold",
-      hero: "bg-gray-900 text-gray-50",
-      item: "text-gray-500 hover:text-gray-300",
+});
+
+const breadcrumbListVariants = cva("flex text-sm overflow-x-auto", {
+  variants: { prose: { true: "max-w-min mx-auto justify-center" } },
+});
+
+const breadcrumbInactiveItemVariants = cva(
+  "whitespace-nowrap after:content-['→'] after:px-2 after:text-gray-400 capitalize transition",
+  {
+    variants: {
+      color: {
+        blue: "text-blue-500 hover:text-blue-300",
+        gray: "text-gray-500 hover:text-gray-300",
+        transparent:
+          "text-gray-800 hover:text-gray-600 transition hover:text-blue-500",
+      },
+      prose: { true: "", false: "" },
     },
-  ],
-  [
-    "transparent",
-    {
-      container: "bg-gray-50 border",
-      heading: "font-semibold",
-      hero: "bg-transparent text-gray-900",
-      item: "text-gray-800 hover:text-gray-600 transition hover:text-blue-500",
-    },
-  ],
-]);
+    compoundVariants: [
+      {
+        color: "transparent",
+        prose: true,
+        className: "mx-auto",
+      },
+    ],
+  },
+);
+
+const HeroBreadcrumbs = ({
+  prose,
+  color,
+  title,
+}: Pick<BaseProps, "title"> &
+  VariantProps<typeof breadcrumbContainerVariants>) => (
+  <div className={breadcrumbContainerVariants({ color, prose })}>
+    <Container>
+      <Breadcrumbs
+        listClassName={breadcrumbListVariants({ prose })}
+        rootLabel={ROOT_LABEL}
+        inactiveItemClassName={breadcrumbInactiveItemVariants({ color })}
+        activeItemClassName="whitespace-nowrap font-medium capitalize"
+        currentLabel={title}
+      />
+    </Container>
+  </div>
+);
 
 const HeroTitle = ({
   title,
@@ -57,62 +129,14 @@ const HeroTitle = ({
   breadcrumbs = false,
   color = "blue",
   transparent = false,
-}: Props) => {
+}: BaseProps & VariantProps<typeof headingVariants>) => {
   // For back compatibility with the old `transparent` prop
-  const localColor = transparent ? "transparent" : color;
-
-  const colorClasses = COLOR_MAP.get(localColor);
+  color = transparent ? "transparent" : color;
 
   return (
     <>
-      <div
-        className={cn(
-          "relative flex h-24 flex-wrap content-center sm:h-36 md:h-48",
-          colorClasses?.hero,
-        )}
-      >
-        <Container>
-          <div className={cn(prose && "mx-auto max-w-prose text-center")}>
-            <h1
-              className={cn(
-                "text-4xl sm:text-5xl md:text-6xl",
-                colorClasses?.heading,
-              )}
-            >
-              {title}
-            </h1>
-            {children}
-          </div>
-        </Container>
-      </div>
-      {breadcrumbs && (
-        <div
-          className={cn(
-            "py-2",
-            colorClasses?.container,
-            prose && color === "transparent"
-              ? "mx-auto w-min rounded-full border"
-              : "border-y",
-          )}
-        >
-          <Container>
-            <Breadcrumbs
-              listClassName={cn(
-                "flex text-sm overflow-x-auto",
-                prose && "max-w-min mx-auto justify-center",
-              )}
-              rootLabel="Home"
-              inactiveItemClassName={cn(
-                "whitespace-nowrap after:content-['→'] after:px-2 after:text-gray-400 capitalize transition",
-                colorClasses?.item,
-                prose && color === "transparent" && "mx-auto",
-              )}
-              activeItemClassName="whitespace-nowrap font-medium capitalize"
-              currentLabel={title}
-            />
-          </Container>
-        </div>
-      )}
+      <HeroTitleSubcomponent {...{ color, title, children, prose }} />
+      {breadcrumbs && <HeroBreadcrumbs {...{ color, title, prose }} />}
     </>
   );
 };
