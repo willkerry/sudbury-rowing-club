96f62d7f4366d36cdb71d38e7dc8428e140e1ea4
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-11-17 08:56:26 +0000

    Fix article bottom margin

--- apps/web/app/news/[slug]/layout.tsx
+++ apps/web/app/news/[slug]/layout.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/layouts/container";
 
 const PostLayout = ({ children }: { children: React.ReactNode }) => (
   <Container>
-    <article className="mb-32">{children}</article>
+    <article className="pb-16">{children}</article>
   </Container>
 );
 
