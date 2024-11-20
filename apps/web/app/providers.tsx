eafa152b975f656f0304b5bfdf5fd56d282e358a
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-11-20 19:39:51 +0000

    Add PostHog

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -4,6 +4,20 @@ import { Toaster } from "@/components/ui/sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
 import { getQueryClient } from "./get-query-client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  if (!process.env.NEXT_PUBLIC_POSTHOG_KEY)
+    throw new Error("Missing NEXT_PUBLIC_POSTHOG_KEY");
+  if (!process.env.NEXT_PUBLIC_POSTHOG_HOST)
+    throw new Error("Missing NEXT_PUBLIC_POSTHOG_HOST");
+
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only",
+  });
+}
 
 const ReactQueryDevtools = dynamic(
   () =>
@@ -17,14 +31,16 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        {children}
 
-      {process.env.NODE_ENV === "development" && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
+        {process.env.NODE_ENV === "development" && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
 
-      <Toaster />
-    </QueryClientProvider>
+        <Toaster />
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
