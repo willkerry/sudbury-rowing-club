1f4cc427524903613ff12c6d145404053d584092
Will Kerry <24854452+willkerry@users.noreply.github.com>
2024-12-29 20:56:22 +0000

    Use a dynamic import for contact form testing mode

--- apps/web/components/contact/useTestingMode.tsx
+++ apps/web/components/contact/useTestingMode.tsx
@@ -5,15 +5,26 @@ import { useHotkeys, useOs, useThrottledCallback } from "@mantine/hooks";
 import type { OfficerResponse } from "@sudburyrc/api";
 import type { ReactFormExtendedApi } from "@tanstack/react-form";
 import { useQuery } from "@tanstack/react-query";
-import type { getWodehouseFullDetails } from "get-wodehouse-name";
-import ky from "ky";
 import { useState } from "react";
-import { toast } from "sonner";
+import { type ExternalToast, toast } from "sonner";
+import { Button } from "../ui/button";
 import type { Message } from "./contactForm";
+
+const TESTING_MODE_TOAST_OPTIONS: ExternalToast = {
+  id: "testing-mode",
+  position: "top-center",
+};
 
 export const useTestingMode = (form: ReactFormExtendedApi<Message>) => {
   const os = useOs();
   const [isEnabled, setIsEnabled] = useState(false);
+
+  const { data: getWodehouseFullDetails } = useQuery({
+    enabled: isEnabled,
+    queryKey: ["getWodehouseFullDetails"],
+    queryFn: () => import("get-wodehouse-name"),
+    select: ({ getWodehouseFullDetails }) => getWodehouseFullDetails,
+  });
 
   const { data: webmasterId } = useQuery({
     enabled: isEnabled,
@@ -22,11 +33,10 @@ export const useTestingMode = (form: ReactFormExtendedApi<Message>) => {
     select: (data) => data.hits[0]?._id,
   });
 
-  const { refetch } = useQuery({
-    enabled: isEnabled,
-    queryKey: ["/api/pg"],
-    queryFn: () =>
-      ky.get<ReturnType<typeof getWodehouseFullDetails>>("/api/pg").json(),
+  const { refetch: setFieldValues } = useQuery({
+    enabled: isEnabled && !!getWodehouseFullDetails,
+    queryKey: ["setFieldValues"],
+    queryFn: getWodehouseFullDetails,
     select: ({ firstName, lastName, email }) => {
       const name = `${firstName} ${lastName}`;
 
@@ -34,42 +44,52 @@ export const useTestingMode = (form: ReactFormExtendedApi<Message>) => {
       form.setFieldValue("name", name);
       form.setFieldValue("email", email);
 
-      return { name, email };
+      return undefined;
     },
   });
 
   const handleHotkey = useThrottledCallback(async () => {
     if (isEnabled) {
-      await refetch();
-      toast("Testing mode sender regenerated.", { position: "top-center" });
+      await setFieldValues();
+      toast.success("New sender generated.", {
+        position: "top-center",
+      });
 
       return;
     }
 
     const didConfirm = await new Promise<boolean>((resolve) => {
       toast("Enable testing mode?", {
-        id: "testing-mode",
+        ...TESTING_MODE_TOAST_OPTIONS,
         description: (
           <div className="text-gray-700">
             You can activate testing mode by pressing{" "}
             {os === "macos" ? <kbd>⌘+J</kbd> : <kbd>Ctrl+J</kbd>}.
           </div>
         ),
-        position: "top-center",
-        dismissible: true,
         onDismiss: () => resolve(false),
         onAutoClose: () => resolve(false),
-        action: {
-          label: "Confirm",
-          onClick: () => resolve(true),
-        },
+        action: (
+          <Button onClick={() => resolve(true)} size="xs" autoFocus>
+            Enable
+          </Button>
+        ),
       });
     });
 
     if (!didConfirm) return;
 
+    toast.loading("Loading testing mode...", {
+      ...TESTING_MODE_TOAST_OPTIONS,
+      action: undefined,
+      description: undefined,
+    });
+
     setIsEnabled(true);
-  }, 1000);
+    await setFieldValues();
+
+    toast.success("Testing mode enabled.", TESTING_MODE_TOAST_OPTIONS);
+  }, 300);
 
   useHotkeys([["mod+j", handleHotkey]]);
 };
