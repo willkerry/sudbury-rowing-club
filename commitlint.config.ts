22e9ab71ce3ba1d9fbdb5b4d71f2b95df0d3ad7c
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-05-06 20:53:36 +0100

    feat(monorepo): begin adding mechanisms for auto-releasing

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,22 @@ import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "repo",
+        "web",
+        "cms",
+        "api",
+        "blue",
+        "helpers",
+        "ical-builder",
+        "tsconfig",
+      ],
+    ],
+  },
 };
 
 export default Configuration;
