743e9a34f1eb2f124145a2fb3a768dcea5e73fde
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-05-20 20:16:46 +0100

    fix: remove duplicated AccordionHeader Radix UI primitive

--- apps/web/components/stour/collapsible-card/collapsible-card.tsx
+++ apps/web/components/stour/collapsible-card/collapsible-card.tsx
@@ -9,7 +9,6 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import DateFormatter from "@/components/utils/date-formatter";
-import { AccordionHeader } from "@radix-ui/react-accordion";
 import type { Notice } from "@sudburyrc/api";
 import { LinkIcon } from "lucide-react";
 import Link from "next/link";
@@ -86,16 +85,14 @@ const CollapsibleCard = ({ notice }: Props) => (
     collapsible
   >
     <AccordionItem value={notice.title}>
-      <AccordionHeader>
-        <AccordionTrigger className="group flex h-14 w-full items-center justify-between px-4 text-left transition hover:bg-gray-50 data-[state=open]:bg-gray-50">
-          <Label
-            className="transition duration-300 group-hover:text-black"
-            as="h2"
-          >
-            {notice.title}
-          </Label>
-        </AccordionTrigger>
-      </AccordionHeader>
+      <AccordionTrigger className="group flex h-14 w-full items-center justify-between px-4 text-left transition hover:bg-gray-50 data-[state=open]:bg-gray-50">
+        <Label
+          className="transition duration-300 group-hover:text-black"
+          as="p"
+        >
+          {notice.title}
+        </Label>
+      </AccordionTrigger>
       <AccordionContent className="divide-y border-t p-0">
         <NoticeBody {...{ notice }} />
       </AccordionContent>
