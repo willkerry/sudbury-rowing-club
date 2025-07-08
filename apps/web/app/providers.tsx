d652b573ef63f62f8817567785022ab6176cf0e7
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-07-08 16:18:55 +0100

    feat: set localStorage persistence for PostHog initialisation

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -18,6 +18,7 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only",
+    persistence: "localStorage",
   });
 }
 
