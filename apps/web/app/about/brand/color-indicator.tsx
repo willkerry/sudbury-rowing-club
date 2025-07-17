688e59c188b484b6936c84fdaff2acb1923c1227
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-07-17 20:47:51 +0100

    refactor: nod to FP

--- apps/web/app/about/brand/color-indicator.tsx
+++ apps/web/app/about/brand/color-indicator.tsx
@@ -3,6 +3,14 @@ import { Circle } from "lucide-react";
 import { tryit } from "radash";
 import { Copy } from "@/components/stour/copy";
 
+const getPrint = (color: string, type: string) => {
+  const [_, newColor] = tryit(() => Color(color))();
+
+  if (newColor && type === "rgb") return newColor.rgb().string();
+  if (newColor && type === "hsl") return newColor.hsl().round().string();
+  return color;
+};
+
 export const ColorIndicator = ({
   color = "rgb",
   type,
@@ -10,14 +18,11 @@ export const ColorIndicator = ({
   color: string;
   type: string;
 }) => {
-  const [_, newColor] = tryit(() => Color(color))();
+  const print = getPrint(color, type);
 
-  let print = color;
-  if (newColor && type === "rgb") print = newColor.rgb().string();
-  if (newColor && type === "hsl") print = newColor.hsl().round().string();
   return (
     <span className="flex flex-row items-center text-xs">
-      {newColor ? (
+      {print ? (
         <>
           <Circle className="h-4 text-gray-200" fill={color} />
           <Copy value={print.toUpperCase()} />
