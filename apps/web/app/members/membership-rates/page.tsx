58587945aae143284c46fdbfede67882e4d43038
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-07-29 19:16:03 +0100

    fix: increase revalidation period for membership rates page from 60 seconds to 24 hours

--- apps/web/app/members/membership-rates/page.tsx
+++ apps/web/app/members/membership-rates/page.tsx
@@ -4,7 +4,7 @@ import { Error as ErrorComponent } from "@/components/ui/error";
 import { createMetadata } from "@/lib/create-metadata";
 import { scrapeRatesTable } from "@/lib/scrapeRatesTable";
 
-export const revalidate = 60;
+export const revalidate = 86400;
 
 export const metadata = createMetadata({
   title: "Membership Rates",
