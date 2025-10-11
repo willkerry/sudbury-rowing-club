24f40f6b2d899e35f08d513789d1f493dac21921
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-10-11 20:13:23 +0100

    feat: add perf logging to results build script

--- apps/results/src/build.ts
+++ apps/results/src/build.ts
@@ -1,6 +1,8 @@
+import { performance } from "node:perf_hooks";
 import { Parcel } from "@parcel/core";
 import { consola } from "consola";
 import { remove } from "fs-extra/esm";
+import ms from "ms";
 
 import { cleanResults, generateResults, TEMP } from "./convert.ts";
 
@@ -8,6 +10,8 @@ const clean = async () =>
   Promise.all([cleanResults(), remove(".parcel-cache")]);
 
 export const build = async () => {
+  const start = performance.now();
+
   const bundler = new Parcel({
     entries: `${TEMP}/index.html`,
     defaultConfig: "@parcel/config-default",
@@ -16,16 +20,23 @@ export const build = async () => {
 
   try {
     await clean();
+    consola.info(`Cleaned up old results in ${ms(performance.now() - start)}`);
 
     consola.start("Generating results from archival HTML...");
 
     await generateResults();
+    consola.info(`Generated results in ${ms(performance.now() - start)}`);
 
     consola.start("Bundling results with Parcel...");
 
     await bundler.run();
+    consola.info(
+      `Bundled results with Parcel in ${ms(performance.now() - start)}`,
+    );
 
-    consola.success("Done.");
+    const end = performance.now();
+
+    consola.success(`Done in ${ms(end - start)}`);
   } catch (error) {
     consola.error(error);
   } finally {
