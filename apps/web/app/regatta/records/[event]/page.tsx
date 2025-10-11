906b59ce5b4f682237d70b92edaee3808bc2e24f
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-10-11 22:26:15 +0100

    feat: use extended event record names in page title

--- apps/web/app/regatta/records/[event]/page.tsx
+++ apps/web/app/regatta/records/[event]/page.tsx
@@ -5,6 +5,7 @@ import { getResultBySlug, getSlugifiedRecords } from "../transformRecords";
 import { formatDescription } from "./format-description";
 import { RecordHolderList } from "./record-holder-list";
 import { RecordTime } from "./record-time";
+import { extendEventName } from "./utils";
 
 type Param = { event: string };
 
@@ -21,7 +22,7 @@ export const generateMetadata = async ({
   }
 
   return createMetadata({
-    title: `Course record for ${records[0].event}`,
+    title: `Course record for ${extendEventName(records[0].event)}`,
     description: formatDescription(records),
     publishedTime: records[0].year.toISOString(),
     type: "article",
@@ -51,7 +52,7 @@ const Page = async ({ params }: { params: Promise<Param> }) => {
   const description = formatDescription(records);
 
   return (
-    <TextPage title={eventName} prose="prose">
+    <TextPage title={extendEventName(eventName)} prose="prose">
       <RecordTime time={time} />
       <RecordHolderList records={records} />
 
