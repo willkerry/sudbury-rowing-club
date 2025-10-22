ea747f5c8529c8da0cb61e0dd0b88776736accbe
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-10-22 19:52:20 +0100

    fix: join page scroll links

--- apps/web/app/join/page.tsx
+++ apps/web/app/join/page.tsx
@@ -1,14 +1,15 @@
 import { ThumbsUp } from "lucide-react";
 import Image from "next/image";
 import NextLink from "next/link";
+import { useId } from "react";
 import JoinImage from "@/assets/join/l2r.jpg";
 import {
-  JoinCTA,
   L2RCallToAction,
   L2RStages,
   Overview,
   SectionHeading,
 } from "@/components/join";
+import { LearnToRowCta, LearnToRowCtaSection } from "@/components/join/cta";
 import { Container } from "@/components/layouts/container";
 import { Label } from "@/components/stour/label";
 import { Link } from "@/components/stour/link";
@@ -55,132 +56,156 @@ const templateLearnToRowCourseCost = (cost: string | null) =>
     ? `The programme costs ${cost} and for its duration, you’ll be a club member.`
     : "For the duration of the programme, you’ll be a club member.";
 
-const Join = async () => (
-  <>
-    {/* Big hero */}
-    <Container className="py-16 text-center text-gray-900 sm:py-24">
-      <Label className="sm:mb-3">Join Sudbury Rowing Club</Label>
-      <h1 className="relative z-10 font-semibold text-6xl text-green-500 tracking-tighter drop-shadow-xl sm:text-8xl">
-        Start rowing with us{" "}
-        <ThumbsUp className="inline h-12 w-12 rotate-3 text-blue-400 sm:h-16 sm:w-16" />
-      </h1>
-      <div className="-rotate-2 z-0 mx-auto flex max-h-[160px] max-w-[240px] overflow-hidden rounded-sm shadow-xl">
-        <Image
-          alt="Trainee rowers on the water during on learn to row course."
-          height={160}
-          placeholder="blur"
-          src={JoinImage}
-          width={240}
-        />
-      </div>
-      <div className="prose mx-auto mt-12 mb-16 max-w-xl text-left">
-        <p className="lead">
-          Sudbury Rowing Club welcomes new members
-          <span className="whitespace-nowrap"> –</span> whether you’ve never
-          rowed before and are seeking a new activity, or have rowed in the past
-          and would like to return to the sport.
-        </p>
-        <p>
-          We have two routes to club membership and only one essential criterion
-          – that you can swim 50m.
-        </p>
-      </div>
-      <JoinCTA />
-    </Container>
+const Join = async () => {
+  const learnToRowId = useId();
+  const experienceId = useId();
 
-    <Container>
-      <SectionHeading
-        label="Join as an experienced rower"
-        title="Have you rowed before?"
-      />
-      <div className="mb-20 grid gap-12 md:grid-cols-2">
-        <div className="prose">
+  return (
+    <>
+      {/* Big hero */}
+      <Container className="py-16 text-center text-gray-900 sm:py-24">
+        <Label className="sm:mb-3">Join Sudbury Rowing Club</Label>
+        <h1 className="relative z-10 font-semibold text-6xl text-green-500 tracking-tighter drop-shadow-xl sm:text-8xl">
+          Start rowing with us{" "}
+          <ThumbsUp className="inline h-12 w-12 rotate-3 text-blue-400 sm:h-16 sm:w-16" />
+        </h1>
+        <div className="-rotate-2 z-0 mx-auto flex max-h-[160px] max-w-[240px] overflow-hidden rounded-sm shadow-xl">
+          <Image
+            alt="Trainee rowers on the water during on learn to row course."
+            height={160}
+            placeholder="blur"
+            src={JoinImage}
+            width={240}
+          />
+        </div>
+        <div className="prose mx-auto mt-12 mb-16 max-w-xl text-left">
           <p className="lead">
-            If you’ve rowed before, you can join the club immediately.
+            Sudbury Rowing Club welcomes new members
+            <span className="whitespace-nowrap"> –</span> whether you’ve never
+            rowed before and are seeking a new activity, or have rowed in the
+            past and would like to return to the sport.
           </p>
           <p>
-            You’ll need to complete a short application form on our membership
-            portal. We offer a considerable variety of membership rates (e.g.
-            short term, student, family, etc.) and you can find out more about
-            these{" "}
-            <Link href="/members/membership-rates">on our membership page</Link>
-            .
-          </p>
-          <p className="font-medium">
-            Your first step should be to contact the club captain.
+            We have two routes to club membership and only one essential
+            criterion – that you can swim 50m.
           </p>
         </div>
-        <div className="flex items-center rounded-sm border border-gray-200 bg-gray-50 p-4 text-center">
-          <div className="w-full">
-            <div className="mb-2 font-medium text-lg">
-              Contact the club captain
-            </div>
-            <Button asChild size="lg">
-              <NextLink href="./contact?q=captain">Contact</NextLink>
-            </Button>
-          </div>
-        </div>
-      </div>
-    </Container>
+        <LearnToRowCta>
+          <LearnToRowCtaSection
+            title="New to the sport?"
+            buttonText="Learn to Row"
+            variant="secondary"
+            to={learnToRowId}
+          >
+            Our Learn to Row programme is for you.
+          </LearnToRowCtaSection>
 
-    <Container className="my-40 flex items-center">
-      <div className="grow border-gray-300 border-t" />
-      <div className="mb-px px-4 font-medium text-lg leading-none">or</div>
-      <div className="grow border-gray-300 border-t" />
-    </Container>
+          <LearnToRowCtaSection
+            title="Already an active rower?"
+            buttonText="How to join"
+            to={experienceId}
+          >
+            Contact a vice-captain for more information.
+          </LearnToRowCtaSection>
+        </LearnToRowCta>
+      </Container>
 
-    {/* About the L2R course */}
-    <div className="my-24">
-      <Container>
+      <Container id={experienceId}>
         <SectionHeading
-          label="Learn to row with us"
-          title="Getting started in rowing couldn’t be easier"
+          label="Join as an experienced rower"
+          title="Have you rowed before?"
         />
-
         <div className="mb-20 grid gap-12 md:grid-cols-2">
           <div className="prose">
             <p className="lead">
-              Our Learn to Row progamme starts with a ‘taster session’ after
-              which you can apply for a place on one of our courses during which
-              you will learn the basics of sculling (an oar in each hand) over
-              approximately 4 × 3-hour sessions.
+              If you’ve rowed before, you can join the club immediately.
             </p>
             <p>
-              We plan to run the Learn to Row programme on a group basis for
-              adults over the next year. Attending a taster session is an
-              opportunity to find out about rowing and the course. You don’t
-              have to make a commitment to the course until after the taster
-              session when you can apply for a place on one of the upcoming
-              courses. Subject to availability you will be allocated a start
-              date along with a number of other participants at a similar level.
-              Juniors (under 18) will be separately invited to programmed
-              courses.
+              You’ll need to complete a short application form on our membership
+              portal. We offer a considerable variety of membership rates (e.g.
+              short term, student, family, etc.) and you can find out more about
+              these{" "}
+              <Link href="/members/membership-rates">
+                on our membership page
+              </Link>
+              .
             </p>
-            <p>
-              {templateLearnToRowCourseCost(await getLearnToRowCourseCost())}{" "}
-              The programme fee includes your first month’s adult membership to
-              the club after which you will be given the option to continue your
-              membership at the adult rate of £32 per month.
+            <p className="font-medium">
+              Your first step should be to contact the club captain.
             </p>
           </div>
-          <div className="flex items-center">
-            <NextCourse />
+          <div className="flex items-center rounded-sm border border-gray-200 bg-gray-50 p-4 text-center">
+            <div className="w-full">
+              <div className="mb-2 font-medium text-lg">
+                Contact the club captain
+              </div>
+              <Button asChild size="lg">
+                <NextLink href="./contact?q=captain">Contact</NextLink>
+              </Button>
+            </div>
           </div>
         </div>
+      </Container>
 
-        <L2RStages stages={l2rStages} />
+      <Container className="my-40 flex items-center">
+        <div className="grow border-gray-300 border-t" />
+        <div className="mb-px px-4 font-medium text-lg leading-none">or</div>
+        <div className="grow border-gray-300 border-t" />
+      </Container>
+
+      {/* About the L2R course */}
+      <div className="my-24" id={learnToRowId}>
+        <Container>
+          <SectionHeading
+            label="Learn to row with us"
+            title="Getting started in rowing couldn’t be easier"
+          />
+
+          <div className="mb-20 grid gap-12 md:grid-cols-2">
+            <div className="prose">
+              <p className="lead">
+                Our Learn to Row progamme starts with a ‘taster session’ after
+                which you can apply for a place on one of our courses during
+                which you will learn the basics of sculling (an oar in each
+                hand) over approximately 4 × 3-hour sessions.
+              </p>
+              <p>
+                We plan to run the Learn to Row programme on a group basis for
+                adults over the next year. Attending a taster session is an
+                opportunity to find out about rowing and the course. You don’t
+                have to make a commitment to the course until after the taster
+                session when you can apply for a place on one of the upcoming
+                courses. Subject to availability you will be allocated a start
+                date along with a number of other participants at a similar
+                level. Juniors (under 18) will be separately invited to
+                programmed courses.
+              </p>
+              <p>
+                {templateLearnToRowCourseCost(await getLearnToRowCourseCost())}{" "}
+                The programme fee includes your first month’s adult membership
+                to the club after which you will be given the option to continue
+                your membership at the adult rate of £32 per month.
+              </p>
+            </div>
+            <div className="flex items-center">
+              <NextCourse />
+            </div>
+          </div>
+
+          <L2RStages stages={l2rStages} />
+          <L2RCallToAction />
+        </Container>
+      </div>
+      <Container className="text-gray-800">
+        <SectionHeading
+          label="What to expect"
+          title="What can you do at Sudbury Rowing Club?"
+        />
+        <Overview items={overview} />
         <L2RCallToAction />
       </Container>
-    </div>
-    <Container className="text-gray-800">
-      <SectionHeading
-        label="What to expect"
-        title="What can you do at Sudbury Rowing Club?"
-      />
-      <Overview items={overview} />
-      <L2RCallToAction />
-    </Container>
-  </>
-);
+    </>
+  );
+};
 
 export default Join;
--- apps/web/components/join/cta.tsx
+++ apps/web/components/join/cta.tsx
@@ -1,67 +1,49 @@
 import type { ComponentProps } from "react";
 import { Button } from "@/components/ui/button";
 import { ScrollLink } from "@/components/utils/scroll-link";
+import { cn } from "@/lib/utils";
 
-type CTALinkProps = {
+type LearnToRowCtaProps = {
   to: string;
   variant?: ComponentProps<typeof Button>["variant"];
   children: React.ReactNode;
+  title: string;
+  buttonText: string;
 };
-type CTATextProps = { first: string; second: string };
 
-const CTALink = ({ to, variant, children }: CTALinkProps) => (
-  <div className="flex justify-center">
-    <ScrollLink duration={300} offset={-30} smooth spy to={to}>
-      <Button size="lg" variant={variant}>
-        {children}
-      </Button>
-    </ScrollLink>
-  </div>
-);
-
-const CTAText = ({ first, second }: CTATextProps) => (
-  <p className="text-center">
-    <span className="font-medium text-gray-800 text-xl">{first}</span>
-    <br />
-    <span className="text-gray-500">{second}</span>
-  </p>
-);
-
-const CTADivider = () => <span className="block h-6" />;
-
-const CTASection = ({
+export const LearnToRowCtaSection = ({
   to,
   variant,
-  first,
-  second,
+  title,
+  buttonText,
   children,
-}: CTALinkProps & CTATextProps) => (
+}: LearnToRowCtaProps) => (
   <div>
-    <CTAText first={first} second={second} />
-    <CTADivider />
-    <CTALink to={to} variant={variant}>
-      {children}
-    </CTALink>
+    <p className="text-center">
+      <span className="font-medium text-gray-800 text-xl">{title}</span>
+      <br />
+      <span className="text-gray-500">{children}</span>
+    </p>
+    <span className="block h-6" />
+    <div className="flex justify-center">
+      <ScrollLink duration={300} offset={-30} smooth spy to={to}>
+        <Button size="lg" variant={variant}>
+          {buttonText}
+        </Button>
+      </ScrollLink>
+    </div>
   </div>
 );
 
-export const JoinCTA = () => (
-  <div className="grid gap-12 rounded-sm border bg-gray-50 p-12 md:grid-cols-2">
-    <CTASection
-      to="l2r"
-      variant="secondary"
-      first="New to the sport?"
-      second="Our Learn to Row programme is for you."
-    >
-      Learn to Row
-    </CTASection>
-
-    <CTASection
-      to="experienced"
-      first="Already an active rower?"
-      second="Contact a vice-captain for more information."
-    >
-      How to join
-    </CTASection>
-  </div>
+export const LearnToRowCta = ({
+  className,
+  ...rest
+}: React.ComponentProps<"div">) => (
+  <div
+    className={cn(
+      "grid gap-12 rounded-sm border bg-gray-50 p-12 md:grid-cols-2",
+      className,
+    )}
+    {...rest}
+  />
 );
