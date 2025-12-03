6778a28aed98e46e7cff8983700e7094ccdf8df5
Will Kerry <24854452+willkerry@users.noreply.github.com>
2025-12-03 20:34:11 +0000

    feat: add redux caching

--- apps/web/app/api/forecast/route.ts
+++ apps/web/app/api/forecast/route.ts
@@ -1,19 +1,23 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { getWeatherForecast } from "@/lib/get-weather-forecast";
 import { routeHandlerRatelimiter } from "@/lib/rate-limiter";
-
-export const revalidate = 300;
+import { cached } from "@/lib/server/cached";
 
 export const GET = async (req: NextRequest) => {
   const maybeRateLimitedResponse = await routeHandlerRatelimiter(req);
   if (maybeRateLimitedResponse) return maybeRateLimitedResponse;
 
   try {
-    const forecast = await getWeatherForecast();
-    return NextResponse.json(forecast);
-  } catch {
-    return new NextResponse("Server error: failed to fetch weather forecast", {
-      status: 500,
+    const { data: forecast, headers } = await cached({
+      key: "weather-forecast",
+      ttl: 60 * 60 * 12, // 12 hours
+      fn: getWeatherForecast,
     });
+    return NextResponse.json(forecast, { headers });
+  } catch (error) {
+    return NextResponse.json(
+      { error: error instanceof Error ? error.message : String(error) },
+      { status: 500 },
+    );
   }
 };
