f10f4af54bf00f3d02a5132dd59a200582839b3d
Will Kerry <24854452+willkerry@users.noreply.github.com>
2026-03-17 14:25:10 +0000

    feat: add 'active' animation to button

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { Slot as SlotPrimitive } from "radix-ui";
 import { forwardRef } from "react";
 
 const buttonVariants = cva(
-  "not-prose relative inline-flex items-center justify-center whitespace-nowrap rounded-sm font-medium text-sm ring-offset-white transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "not-prose relative inline-flex items-center justify-center whitespace-nowrap rounded-sm font-medium text-sm ring-offset-white transition-[color,background-color,box-shadow,transform] duration-150 focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 active:translate-y-px disabled:pointer-events-none disabled:opacity-50",
   {
     defaultVariants: {
       size: "default",
